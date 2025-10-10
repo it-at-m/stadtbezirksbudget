3fe94b6a1e62df180905685fb4eb5e2f7fac41c6
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-10 10:09:15 +0200

    :bulb: Comment Kafka classes

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/kafka/KafkaConsumerService.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/kafka/KafkaConsumerService.java
@@ -8,9 +8,19 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+/**
+ * Service for consuming and processing messages from a Kafka topic.
+ * This service listens to the 'sbb-eai-topic' Kafka topic and processes incoming messages of type KafkaDTO.
+ */
 @Component
 @Slf4j
 public class KafkaConsumerService {
+    /**
+     * Receives and processes messages from the Kafka topic 'sbb-eai-topic'.
+     *
+     * @param key the key of the received Kafka message as String
+     * @param content the received message as KafkaDTO
+     */
     @KafkaListener(topics = "sbb-eai-topic", groupId = "sbb-backend")
     public void listen(@Header(name = KafkaHeaders.RECEIVED_KEY) String key, KafkaDTO content) {
         UUID uuidKey = UUID.fromString(key);
--- stadtbezirksbudget-eai/src/main/java/de/muenchen/stadtbezirksbudget/kafka/KafkaProducerController.java
+++ stadtbezirksbudget-eai/src/main/java/de/muenchen/stadtbezirksbudget/kafka/KafkaProducerController.java
@@ -7,16 +7,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for publishing messages to Kafka.
+ * Provides an endpoint for sending messages to a Kafka topic via the KafkaProducerService.
+ * Only used for testing purposes. In production, messages will be sent directly from the service layer.
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/kafka")
 public class KafkaProducerController {
     private final KafkaProducerService kafkaProducerService;
 
-    private static final String TOPIC = "sbb-eai-topic";
-    @Autowired
-    private KafkaTemplate<String, KafkaDTO> kafkaTemplate;
-
+    /**
+     * Publishes a message to Kafka.
+     *
+     * @param message the message to be published as KafkaDTO
+     * @return confirmation text that the message was published successfully
+     */
     @PostMapping("/publish")
     public String publishMessage(@RequestBody KafkaDTO message) {
         kafkaProducerService.publishMessage(message);
