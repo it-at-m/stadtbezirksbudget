3c80bff1b87f3f7162c3c26c80f52b381c824626
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-10 09:45:16 +0200

    :loud_sound: Use default logging

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/kafka/KafkaConsumer.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/kafka/KafkaConsumer.java
@@ -2,21 +2,18 @@ package de.muenchen.stadtbezirksbudget.kafka;
 
 import de.muenchen.stadtbezirksbudget.common.KafkaDTO;
 import java.util.UUID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class KafkaConsumer {
-
-    private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
-
     @KafkaListener(topics = "sbb-eai-topic", groupId = "sbb-backend")
     public void listen(@Header(name = KafkaHeaders.RECEIVED_KEY) String key, KafkaDTO content) {
         UUID uuidKey = UUID.fromString(key);
-        LOG.info("Received message in group sbb-backend with key <" + uuidKey + ">: " + content.toString());
+        log.info("Received message in group sbb-backend with key {}: {}", uuidKey, content.toString());
     }
 }
