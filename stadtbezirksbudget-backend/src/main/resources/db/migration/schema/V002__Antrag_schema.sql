0f0963d975d20221ca83547e7533d06097c9da66
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-13 16:31:36 +0200

    🚧 Adapt sql schema

--- stadtbezirksbudget-backend/src/main/resources/db/migration/schema/V002__Antrag_schema.sql
+++ stadtbezirksbudget-backend/src/main/resources/db/migration/schema/V002__Antrag_schema.sql
@@ -10,15 +10,15 @@ CREATE TABLE adresse
 
 CREATE TABLE antrag
 (
-    id                                   UUID    NOT NULL,
-    bezirksausschuss_nr                  INTEGER NOT NULL,
+    id                                   UUID         NOT NULL,
+    bezirksausschuss_nr                  INTEGER      NOT NULL,
     eingangsdatum                        TIMESTAMP WITHOUT TIME ZONE,
-    zuwendungen_dritter_beschreibung     VARCHAR(255),
-    ist_person_vorsteuerabzugsberechtigt BOOLEAN NOT NULL,
-    projekt_id                           UUID,
-    finanzierung_id                      UUID,
-    antragsteller_id                     UUID,
-    bankverbindung_id                    UUID,
+    zuwendungen_dritter_beschreibung     VARCHAR(255) NOT NULL,
+    ist_person_vorsteuerabzugsberechtigt BOOLEAN      NOT NULL,
+    projekt_id                           UUID         NOT NULL,
+    finanzierung_id                      UUID         NOT NULL,
+    antragsteller_id                     UUID         NOT NULL,
+    bankverbindung_id                    UUID         NOT NULL,
     vertretungsberechtigter_id           UUID,
     CONSTRAINT pk_antrag PRIMARY KEY (id)
 );
@@ -29,37 +29,25 @@ CREATE TABLE antrag_mitglieder
     mitglieder_id UUID NOT NULL
 );
 
-CREATE TABLE antragsteller
-(
-    id          UUID NOT NULL,
-    name        VARCHAR(255),
-    telefon_nr  VARCHAR(255),
-    email       VARCHAR(255),
-    zielsetzung VARCHAR(255),
-    rechtsform  SMALLINT,
-    adresse_id  UUID,
-    CONSTRAINT pk_antragsteller PRIMARY KEY (id)
-);
-
 CREATE TABLE bankverbindung
 (
-    id           UUID NOT NULL,
-    person       VARCHAR(255),
-    geldinstitut VARCHAR(255),
-    iban         VARCHAR(255),
-    bic          VARCHAR(255),
-    adresse_id   UUID,
+    id                    UUID NOT NULL,
+    zahlungsempfaenger_id UUID NOT NULL,
+    person                VARCHAR(255),
+    geldinstitut          VARCHAR(255),
+    iban                  VARCHAR(255),
+    bic                   VARCHAR(255),
     CONSTRAINT pk_bankverbindung PRIMARY KEY (id)
 );
 
 CREATE TABLE finanzierung
 (
-    id                                    UUID             NOT NULL,
-    ist_projekt_vorsteuerabzugsberechtigt BOOLEAN          NOT NULL,
-    bewilligter_zuschuss                  DOUBLE PRECISION NOT NULL,
-    ist_einladungs_foerderhinweis         BOOLEAN          NOT NULL,
-    ist_website_foerderhinweis            BOOLEAN          NOT NULL,
-    sonstige_foerderhinweis               VARCHAR(255),
+    id                                    UUID         NOT NULL,
+    ist_projekt_vorsteuerabzugsberechtigt BOOLEAN      NOT NULL,
+    bewilligter_zuschuss                  DOUBLE PRECISION,
+    ist_einladungs_foerderhinweis         BOOLEAN      NOT NULL,
+    ist_website_foerderhinweis            BOOLEAN      NOT NULL,
+    sonstige_foerderhinweise              VARCHAR(255) NOT NULL,
     CONSTRAINT pk_finanzierung PRIMARY KEY (id)
 );
 
@@ -80,7 +68,7 @@ CREATE TABLE finanzierungsmittel
     id                UUID             NOT NULL,
     kategorie         SMALLINT,
     betrag            DOUBLE PRECISION NOT NULL,
-    direktorium_notiz VARCHAR(255),
+    direktorium_notiz VARCHAR(255)     NOT NULL,
     CONSTRAINT pk_finanzierungsmittel PRIMARY KEY (id)
 );
 
@@ -89,7 +77,7 @@ CREATE TABLE mitglied
     id         UUID NOT NULL,
     nachname   VARCHAR(255),
     vorname    VARCHAR(255),
-    adresse_id UUID,
+    adresse_id UUID NOT NULL,
     CONSTRAINT pk_mitglied PRIMARY KEY (id)
 );
 
@@ -103,26 +91,43 @@ CREATE TABLE projekt
     CONSTRAINT pk_projekt PRIMARY KEY (id)
 );
 
-CREATE TABLE vertretungsberechtigter
-(
-    id         UUID NOT NULL,
-    nachname   VARCHAR(255),
-    vorname    VARCHAR(255),
-    telefon_nr VARCHAR(255),
-    email      VARCHAR(255),
-    mobil_nr   VARCHAR(255),
-    adresse_id UUID,
-    CONSTRAINT pk_vertretungsberechtigter PRIMARY KEY (id)
-);
-
 CREATE TABLE voraussichtliche_ausgabe
 (
     id                UUID             NOT NULL,
     kategorie         VARCHAR(255),
     betrag            DOUBLE PRECISION NOT NULL,
-    direktorium_notiz VARCHAR(255),
+    direktorium_notiz VARCHAR(255)     NOT NULL,
     CONSTRAINT pk_voraussichtlicheausgabe PRIMARY KEY (id)
 );
+
+CREATE TABLE zahlungsempfaenger
+(
+    id          UUID         NOT NULL,
+    dtype       VARCHAR(31),
+    telefon_nr  VARCHAR(255),
+    email       VARCHAR(255),
+    adresse_id  UUID         NOT NULL,
+    nachname    VARCHAR(255),
+    vorname     VARCHAR(255),
+    mobil_nr    VARCHAR(255) NOT NULL,
+    name        VARCHAR(255),
+    zielsetzung VARCHAR(255) NOT NULL,
+    rechtsform  SMALLINT,
+    CONSTRAINT pk_zahlungsempfaenger PRIMARY KEY (id)
+);
+
+ALTER TABLE bankverbindung
+    ADD CONSTRAINT uc_79b9fc5faada2c50fc7a5cae5 UNIQUE (person, geldinstitut, iban, bic, zahlungsempfaenger_id);
+
+ALTER TABLE zahlungsempfaenger
+    ADD CONSTRAINT uc_8ec5b2df9546feafd9ef863a7 UNIQUE (dtype, email, adresse_id, nachname, vorname, name, zielsetzung,
+                                                        rechtsform);
+
+ALTER TABLE adresse
+    ADD CONSTRAINT uc_cff3c04d7299fe58dd0a0dfef UNIQUE (strasse, hausnummer, ort, postleitzahl);
+
+ALTER TABLE projekt
+    ADD CONSTRAINT uc_de31f3372638318247e13c50a UNIQUE (titel, beschreibung, start, ende);
 
 ALTER TABLE finanzierung_finanzierungsmittel_liste
     ADD CONSTRAINT uc_finanzierungfinanzierungsmittellist_finanzierungsmittelliste UNIQUE (finanzierungsmittel_liste_id);
@@ -130,11 +135,8 @@ ALTER TABLE finanzierung_finanzierungsmittel_liste
 ALTER TABLE finanzierung_voraussichtliche_ausgaben
     ADD CONSTRAINT uc_finanzierungvoraussichtlicheausgabe_voraussichtlicheausgaben UNIQUE (voraussichtliche_ausgaben_id);
 
-ALTER TABLE antragsteller
-    ADD CONSTRAINT FK_ANTRAGSTELLER_ON_ADRESSE FOREIGN KEY (adresse_id) REFERENCES adresse (id);
-
 ALTER TABLE antrag
-    ADD CONSTRAINT FK_ANTRAG_ON_ANTRAGSTELLER FOREIGN KEY (antragsteller_id) REFERENCES antragsteller (id);
+    ADD CONSTRAINT FK_ANTRAG_ON_ANTRAGSTELLER FOREIGN KEY (antragsteller_id) REFERENCES zahlungsempfaenger (id);
 
 ALTER TABLE antrag
     ADD CONSTRAINT FK_ANTRAG_ON_BANKVERBINDUNG FOREIGN KEY (bankverbindung_id) REFERENCES bankverbindung (id);
@@ -146,16 +148,16 @@ ALTER TABLE antrag
     ADD CONSTRAINT FK_ANTRAG_ON_PROJEKT FOREIGN KEY (projekt_id) REFERENCES projekt (id);
 
 ALTER TABLE antrag
-    ADD CONSTRAINT FK_ANTRAG_ON_VERTRETUNGSBERECHTIGTER FOREIGN KEY (vertretungsberechtigter_id) REFERENCES vertretungsberechtigter (id);
+    ADD CONSTRAINT FK_ANTRAG_ON_VERTRETUNGSBERECHTIGTER FOREIGN KEY (vertretungsberechtigter_id) REFERENCES zahlungsempfaenger (id);
 
 ALTER TABLE bankverbindung
-    ADD CONSTRAINT FK_BANKVERBINDUNG_ON_ADRESSE FOREIGN KEY (adresse_id) REFERENCES adresse (id);
+    ADD CONSTRAINT FK_BANKVERBINDUNG_ON_ZAHLUNGSEMPFAENGER FOREIGN KEY (zahlungsempfaenger_id) REFERENCES zahlungsempfaenger (id);
 
 ALTER TABLE mitglied
     ADD CONSTRAINT FK_MITGLIED_ON_ADRESSE FOREIGN KEY (adresse_id) REFERENCES adresse (id);
 
-ALTER TABLE vertretungsberechtigter
-    ADD CONSTRAINT FK_VERTRETUNGSBERECHTIGTER_ON_ADRESSE FOREIGN KEY (adresse_id) REFERENCES adresse (id);
+ALTER TABLE zahlungsempfaenger
+    ADD CONSTRAINT FK_ZAHLUNGSEMPFAENGER_ON_ADRESSE FOREIGN KEY (adresse_id) REFERENCES adresse (id);
 
 ALTER TABLE antrag_mitglieder
     ADD CONSTRAINT fk_antmit_on_antrag FOREIGN KEY (antrag_id) REFERENCES antrag (id);
