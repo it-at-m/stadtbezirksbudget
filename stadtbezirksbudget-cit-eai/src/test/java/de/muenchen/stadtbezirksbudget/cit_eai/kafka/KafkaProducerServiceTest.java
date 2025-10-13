fc68e9681582b7a8d4cf6dff35bfa667a01cf7a5
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-13 15:41:18 +0200

    :white_check_mark: Fix null topic in test

--- stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/kafka/KafkaProducerServiceTest.java
+++ stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/kafka/KafkaProducerServiceTest.java
@@ -2,6 +2,7 @@ package de.muenchen.stadtbezirksbudget.cit_eai.kafka;
 
 import static org.mockito.Mockito.verify;
 
+import java.lang.reflect.Field;
 import java.util.UUID;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -22,8 +23,11 @@ class KafkaProducerServiceTest {
     @Nested
     class PublishMessage {
         @Test
-        void testPublishMessageCallsSend() {
+        void testPublishMessageCallsSend() throws Exception {
             final KafkaDTO kafkaDTO = new KafkaDTO(UUID.randomUUID(), "test message", 123);
+            Field topicField = KafkaProducerService.class.getDeclaredField("topic");
+            topicField.setAccessible(true);
+            topicField.set(kafkaProducerService, TOPIC);
             kafkaProducerService.publishMessage(kafkaDTO);
             verify(kafkaTemplate).send(TOPIC, kafkaDTO.id().toString(), kafkaDTO);
         }
