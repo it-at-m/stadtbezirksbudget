a39b721b6be63a41591bf02b4b31db0531f88c3f
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-13 15:26:35 +0200

    :white_check_mark: Use constant test topic instead of injecting

--- stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/kafka/KafkaProducerServiceTest.java
+++ stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/kafka/KafkaProducerServiceTest.java
@@ -9,13 +9,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @ExtendWith(MockitoExtension.class)
 class KafkaProducerServiceTest {
-    @Value("${spring.kafka.template.default-topic}")
-    private String topic;
+    private static final String TOPIC = "test-topic";
     @Mock
     private KafkaTemplate<String, KafkaDTO> kafkaTemplate;
     @InjectMocks
@@ -27,7 +25,7 @@ class KafkaProducerServiceTest {
         void testPublishMessageCallsSend() {
             final KafkaDTO kafkaDTO = new KafkaDTO(UUID.randomUUID(), "test message", 123);
             kafkaProducerService.publishMessage(kafkaDTO);
-            verify(kafkaTemplate).send(topic, kafkaDTO.id().toString(), kafkaDTO);
+            verify(kafkaTemplate).send(TOPIC, kafkaDTO.id().toString(), kafkaDTO);
         }
     }
 }
