21a36a2a9e0f1395243684d7ecc544f2cbec72b4
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-13 16:28:56 +0200

    :rotating_light: Fix pmd warning

--- stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/kafka/KafkaProducerServiceTest.java
+++ stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/kafka/KafkaProducerServiceTest.java
@@ -2,8 +2,8 @@ package de.muenchen.stadtbezirksbudget.cit_eai.kafka;
 
 import static org.mockito.Mockito.verify;
 
-import java.lang.reflect.Field;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class KafkaProducerServiceTest {
@@ -20,14 +21,16 @@ class KafkaProducerServiceTest {
     @InjectMocks
     private KafkaProducerService kafkaProducerService;
 
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(kafkaProducerService, "topic", TOPIC);
+    }
+
     @Nested
     class PublishMessage {
         @Test
-        void testPublishMessageCallsSend() throws Exception {
+        void testPublishMessageCallsSend() {
             final KafkaDTO kafkaDTO = new KafkaDTO(UUID.randomUUID(), "test message", 123);
-            Field topicField = KafkaProducerService.class.getDeclaredField("topic");
-            topicField.setAccessible(true);
-            topicField.set(kafkaProducerService, TOPIC);
             kafkaProducerService.publishMessage(kafkaDTO);
             verify(kafkaTemplate).send(TOPIC, kafkaDTO.id().toString(), kafkaDTO);
         }
