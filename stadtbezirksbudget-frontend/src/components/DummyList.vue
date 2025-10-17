ff1e957b6f863e9489942f7bdd000262443c17f0
bodmer02 <184080611+bodmer02@users.noreply.github.com>
2025-10-17 14:56:35 +0200

    :sparkles: Add table with dummy list

--- stadtbezirksbudget-frontend/src/components/DummyList.vue
+++ stadtbezirksbudget-frontend/src/components/DummyList.vue
@@ -1,5 +1,15 @@
 <template>
   {{ dummyList }}
+    <v-data-table-server
+        v-model:items-per-page="itemsPerPage"
+        :headers="headers"
+        :items="serverItems"
+        :items-length="totalItems"
+        :loading="loading"
+        :search="search"
+        item-value="name"
+        @update:options="loadItems"
+    ></v-data-table-server>
 </template>
 
 <script setup lang="ts">
@@ -7,6 +17,7 @@ import type Dummy from "@/types/Dummy.ts";
 import type Page from "@/types/Page.ts";
 
 import { onMounted, ref } from "vue";
+import type { DataTableHeader } from 'vuetify';
 
 import { getDummyList } from "@/api/fetch-dummy-list.ts";
 import { useSnackbarStore } from "@/stores/snackbar.ts";
@@ -21,4 +32,29 @@ onMounted(() => {
       snackbarStore.showMessage(error);
     });
 });
+
+const itemsPerPage = ref(5)
+const headers = ref<DataTableHeader[]>([
+  { title: 'Name', key: 'name', align: 'start' },
+  { title: 'Email', key: 'email', align: 'start' },
+  { title: 'Alter', key: 'age', align: 'end' },
+  { title: 'Übergewichtig?', key: 'isOverweight', align: 'center' },
+])
+const search = ref('')
+const serverItems = ref<Dummy[]>([])
+const loading = ref(false)
+const totalItems = ref(0)
+async function loadItems ({ page, itemsPerPage}: {page: number, itemsPerPage: number}) {
+  loading.value = true
+  try {
+   const pageResponse: Page<Dummy> = await getDummyList(page - 1, itemsPerPage)
+
+    serverItems.value = pageResponse.content
+    totalItems.value = pageResponse.page.totalElements
+  } catch (error) {
+    snackbarStore.showMessage(error)
+  } finally {
+    loading.value = false
+  }
+}
 </script>
