6d56248eab117dce52dd91ca6a2cac1e02b9a474
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-20 18:45:05 +0200

    :construction: Add DummyAntragsdatenService

---
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/dummy/service/DummyAntragsdatenService.java
@@ -0,0 +1,43 @@
+package de.muenchen.stadtbezirksbudget.backend.dummy.service;
+
+import de.muenchen.stadtbezirksbudget.backend.antrag.dto.AntragsdatenSubsetDTO;
+import de.muenchen.stadtbezirksbudget.backend.antrag.entity.Status;
+import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class DummyAntragsdatenService {
+
+    private final SecureRandom secureRandom = new SecureRandom();
+
+    private AntragsdatenSubsetDTO generateAntragsdaten() {
+        return new AntragsdatenSubsetDTO(getRandomStatus(), secureRandom.nextInt(25), getRandomDate(), "Projekt: " + secureRandom.nextInt(2_000), "AdminAG",
+                secureRandom.nextInt(20_000), "", "admin");
+    }
+
+    private Status getRandomStatus() {
+        return Status.values()[secureRandom.nextInt(Status.values().length)];
+    }
+
+    private LocalDate getRandomDate() {
+        return LocalDate.ofEpochDay(secureRandom.nextInt(10_000));
+    }
+
+    public Page<AntragsdatenSubsetDTO> getAllEntities(final Pageable pageable) {
+        final List<AntragsdatenSubsetDTO> list = Stream.generate(this::generateAntragsdaten).limit(50).toList();
+        final int start = (int) pageable.getOffset();
+        final int end = Math.min(start + pageable.getPageSize(), list.size());
+        final List<AntragsdatenSubsetDTO> subList = list.subList(start, end);
+        return new PageImpl<>(subList, pageable, list.size());
+    }
+}
