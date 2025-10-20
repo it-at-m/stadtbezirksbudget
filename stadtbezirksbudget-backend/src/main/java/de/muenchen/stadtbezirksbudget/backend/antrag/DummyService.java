5d5798b192cb2d87c822f3c5c7177bf8ba05eaf0
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-20 16:56:13 +0200

    :art: Fix pipeline

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/DummyService.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/DummyService.java
@@ -5,7 +5,6 @@ import java.security.SecureRandom;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
