83ca4709f2a5adf5a49c68ac6454f66e6c2cf4bd
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-20 19:02:49 +0200

    :art: Fix format

--- stadtbezirksbudget-frontend/src/api/fetch-antragsdatenSubset-list.ts
+++ stadtbezirksbudget-frontend/src/api/fetch-antragsdatenSubset-list.ts
@@ -5,12 +5,15 @@ import { defaultResponseHandler, getConfig } from "@/api/fetch-utils.ts";
 import { BACKEND } from "@/constants.ts";
 
 export function getDummyList(page: number, size: number): Promise<Page<Dummy>> {
-    return fetch(`${BACKEND}/antragsdatenSubset?page=${page}&size=${size}`, getConfig())
-        .then((response) => {
-            defaultResponseHandler(response);
-            return response.json();
-        })
-        .catch((err) => {
-            defaultResponseHandler(err);
-        });
+  return fetch(
+    `${BACKEND}/antragsdatenSubset?page=${page}&size=${size}`,
+    getConfig()
+  )
+    .then((response) => {
+      defaultResponseHandler(response);
+      return response.json();
+    })
+    .catch((err) => {
+      defaultResponseHandler(err);
+    });
 }
