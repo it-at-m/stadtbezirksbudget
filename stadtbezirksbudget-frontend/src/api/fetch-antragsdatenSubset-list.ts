8a3b5ad1461e140b9d3ad379bd6cd8e07b186c86
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-21 12:51:10 +0200

    :art: Fix formatting

--- stadtbezirksbudget-frontend/src/api/fetch-antragsdatenSubset-list.ts
+++ stadtbezirksbudget-frontend/src/api/fetch-antragsdatenSubset-list.ts
@@ -1,10 +1,13 @@
+import type AntragsdatenSubset from "@/types/AntragsdatenSubset.ts";
 import type Page from "@/types/Page.ts";
 
 import { defaultResponseHandler, getConfig } from "@/api/fetch-utils.ts";
 import { BACKEND } from "@/constants.ts";
-import type AntragsdatenSubset from "@/types/AntragsdatenSubset.ts";
 
-export function getAntragsdatenSubsetList(page: number, size: number): Promise<Page<AntragsdatenSubset>> {
+export function getAntragsdatenSubsetList(
+  page: number,
+  size: number
+): Promise<Page<AntragsdatenSubset>> {
   return fetch(
     `${BACKEND}/antragsdatenSubset?page=${page}&size=${size}`,
     getConfig()
