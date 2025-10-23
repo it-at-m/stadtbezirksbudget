68b7077eced045bd3f2df9d9fbc2c47d2855d40a
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-23 17:20:48 +0200

    :card_file_box: Fix ZahlungsempfaengerRepository

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/repository/ZahlungsempfaengerRepository.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/repository/ZahlungsempfaengerRepository.java
@@ -2,13 +2,10 @@ package de.muenchen.stadtbezirksbudget.backend.antrag.repository;
 
 import de.muenchen.stadtbezirksbudget.backend.antrag.entity.Zahlungsempfaenger;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ZahlungsempfaengerRepository extends PagingAndSortingRepository<Zahlungsempfaenger, UUID>, CrudRepository<Zahlungsempfaenger, UUID> {
-    Page<Zahlungsempfaenger> findByTyp(String typ, Pageable pageable);
 }
