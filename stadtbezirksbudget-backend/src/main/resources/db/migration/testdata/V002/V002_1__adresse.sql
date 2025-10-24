98d2cea04d546a67c6e3b8e8d971e630e3886a2b
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-10-24 09:15:32 +0200

    :wrench: Remove unnecessary address

--- stadtbezirksbudget-backend/src/main/resources/db/migration/testdata/V002/V002_1__adresse.sql
+++ stadtbezirksbudget-backend/src/main/resources/db/migration/testdata/V002/V002_1__adresse.sql
@@ -3,5 +3,4 @@ truncate table adresse, projekt, finanzierung, bearbeitungsstand, voraussichtlic
 INSERT INTO adresse (id, strasse, hausnummer, postleitzahl, ort)
 VALUES ('00000000-0000-0000-0000-000000000000', 'Musterstraße', '1A', '12345', 'Musterstadt'),
        ('00000000-0000-0000-0000-000000000001', 'Beispielweg', '2', '67890', 'Beispielstadt'),
-       ('00000000-0000-0000-0000-000000000002', 'Testallee', '100', '54321', 'Teststadt'),
-       ('00000000-0000-0000-0000-000000000003', 'Demo Boulevard', '50B', '98765', 'Demostadt');
+       ('00000000-0000-0000-0000-000000000002', 'Testallee', '100', '54321', 'Teststadt');
