5ad13269672c0b34ee27c6e0db060ee247871059
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-27 15:28:46 +0100

    :art: enforce formatting

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/AntragSummaryDTO.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/AntragSummaryDTO.java
@@ -4,6 +4,6 @@ import de.muenchen.stadtbezirksbudget.backend.antrag.entity.Status;
 import java.util.UUID;
 
 public record AntragSummaryDTO(UUID id, Status antragsstatus, int bezirksausschussnummer, String eingangsdatum, String projekttitel,
-                               String antragstellerName,
-                               double beantragtesBudget, String anmerkungen, String bearbeiter) {
+        String antragstellerName,
+        double beantragtesBudget, String anmerkungen, String bearbeiter) {
 }
