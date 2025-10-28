45c265eb152a0d79a878727c878197df521db5f6
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-28 09:45:56 +0100

    :construction: Change DTO-attributes in frontend

--- stadtbezirksbudget-frontend/src/types/AntragSummary.ts
+++ stadtbezirksbudget-frontend/src/types/AntragSummary.ts
@@ -3,11 +3,14 @@ import { Status } from "./Status.ts";
 export default interface AntragSummary {
   id: string;
   antragsstatus: Status;
+  zammadNummer: string;
   bezirksausschussnummer: number;
   eingangsdatum: Date;
   projekttitel: string;
   antragstellerName: string;
   beantragtesBudget: number;
+  aktualisierungsArt: string;
+  datumAktualisierung: Date;
   anmerkungen: string;
   bearbeiter: string;
 }
