0a468906e5e96e687258b79f1060a5658c33d48a
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-29 12:59:22 +0100

    :bulb: Fix javadoc

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/repository/ProjektRepository.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/repository/ProjektRepository.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Repository;
 /**
  * Repository for managing {@link } entities.
  * <p>
- * This interface provides basic CRUD operations as well as paging and sorting
- * for {@link } objects.
+ * This interface provides basic CRUD operations for {@link } objects.
  */
 @Repository
 public interface ProjektRepository extends CrudRepository<Projekt, UUID> {
