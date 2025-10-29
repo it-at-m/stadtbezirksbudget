db93236fdddc29496e8cf230f737a180a327edbf
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-29 12:51:21 +0100

    :memo: Document antragSummaryList composable

--- stadtbezirksbudget-frontend/src/composables/antragSummaryList.ts
+++ stadtbezirksbudget-frontend/src/composables/antragSummaryList.ts
@@ -7,6 +7,14 @@ import { getAntragsSummaryList } from "@/api/fetch-antragSummary-list.ts";
 import { STATUS_INDICATORS } from "@/constants.ts";
 import { useSnackbarStore } from "@/stores/snackbar.ts";
 
+/**
+ * Composable function that manages the state and operations for a list of
+ * AntragSummary items. It handles fetching data, pagination, and loading
+ * states, as well as displaying messages through a snackbar.
+ *
+ * @returns {Object} An object exposing the state and methods for managing
+ * AntragSummary items.
+ */
 export function useAntragSummaryList() {
   const snackbarStore = useSnackbarStore();
 
@@ -16,6 +24,11 @@ export function useAntragSummaryList() {
   const itemsPerPage = ref<number>(10);
   const loading = ref<boolean>(false);
 
+  /**
+   * Fetches the AntragSummary items from the API based on the current
+   * page and items per page. Updates the state with the fetched data
+   * and handles any errors by displaying a message in the snackbar.
+   */
   function fetchItems() {
     loading.value = true;
     getAntragsSummaryList(page.value - 1, itemsPerPage.value)
@@ -34,6 +47,13 @@ export function useAntragSummaryList() {
       });
   }
 
+  /**
+   * Updates the pagination options and triggers a fetch for new items.
+   *
+   * @param {Object} options - The new options for pagination.
+   * @param {number} options.page - The new page number.
+   * @param {number} options.itemsPerPage - The new number of items per page.
+   */
   function updateOptions({
     page: newPage,
     itemsPerPage: newItemsPerPage,
@@ -47,10 +67,15 @@ export function useAntragSummaryList() {
   }
 
   return {
+    // List of AntragSummary items.
     items: readonly(items),
+    // Total number of AntragSummary items available.
     totalItems: readonly(totalItems),
+    // Current page number.
     page: readonly(page),
+    // Number of items displayed per page.
     itemsPerPage: readonly(itemsPerPage),
+    // Indicating whether data is currently being loaded.
     loading: readonly(loading),
     fetchItems,
     updateOptions,
