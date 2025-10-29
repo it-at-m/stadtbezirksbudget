effcc6f429367eb0ce46f70595e13ceaf1defb7e
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-29 12:37:34 +0100

    :fire: Remove unused method

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/repository/AndererZuwendungsantragRepository.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/repository/AndererZuwendungsantragRepository.java
@@ -1,7 +1,6 @@
 package de.muenchen.stadtbezirksbudget.backend.antrag.repository;
 
 import de.muenchen.stadtbezirksbudget.backend.antrag.entity.AndererZuwendungsantrag;
-import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -10,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AndererZuwendungsantragRepository
         extends PagingAndSortingRepository<AndererZuwendungsantrag, UUID>, CrudRepository<AndererZuwendungsantrag, UUID> {
-    List<AndererZuwendungsantrag> findByAntragId(UUID antragId);
+
 }
