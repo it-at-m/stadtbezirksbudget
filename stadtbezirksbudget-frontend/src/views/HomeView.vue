ac1a3dec5dee51e3880306e01d29b87a4a96d813
danielgully <74965377+danielgully@users.noreply.github.com>
2025-10-29 16:00:37 +0100

    :fire: Remove unused code

--- stadtbezirksbudget-frontend/src/views/HomeView.vue
+++ stadtbezirksbudget-frontend/src/views/HomeView.vue
@@ -12,31 +12,5 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-
-import { checkHealth } from "@/api/health-client";
 import AntragSummaryList from "@/components/AntragSummaryList.vue";
-import { useSnackbarStore } from "@/stores/snackbar";
-import HealthState from "@/types/HealthState";
-
-const snackbarStore = useSnackbarStore();
-const status = ref("DOWN");
-
-onMounted(() => {
-  checkHealth()
-    .then((content: HealthState) => (status.value = content.status))
-    .catch((error) => {
-      snackbarStore.showMessage(error);
-    });
-});
 </script>
-
-<style scoped>
-.UP {
-  color: limegreen;
-}
-
-.DOWN {
-  color: lightcoral;
-}
-</style>
