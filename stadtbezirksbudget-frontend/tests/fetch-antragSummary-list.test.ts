bdac7cef965e77e2a5c67e080581a24c4417ff5d
danielgully <74965377+danielgully@users.noreply.github.com>
2025-11-03 15:09:35 +0100

    :white_check_mark: Add tests for fetch-antragSummary-list

---
+++ stadtbezirksbudget-frontend/tests/fetch-antragSummary-list.test.ts
@@ -0,0 +1,43 @@
+import { afterEach, describe, expect, test, vi } from "vitest";
+
+import { getAntragsSummaryList } from "@/api/fetch-antragSummary-list.ts";
+import { BACKEND } from "@/constants.ts";
+
+global.fetch = vi.fn();
+
+describe("getAntragsSummaryList", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test("testFetchSuccessfull", async () => {
+    const mockResponse = {
+      content: [
+        /*...*/
+      ],
+      page: { size: 5, number: 1, totalElements: 1, totalPages: 1 },
+    };
+
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      ok: true,
+      json: async () => mockResponse,
+    });
+
+    const result = await getAntragsSummaryList(1, 5);
+
+    expect(fetch).toHaveBeenCalledWith(
+      `${BACKEND}/antrag?page=1&size=5`,
+      expect.any(Object)
+    );
+    expect(result).toEqual(mockResponse);
+  });
+
+  test("testHandleErrorsWhenFetching", async () => {
+    const errorMessage = "Network Error";
+    (fetch as jest.Mock).mockRejectedValueOnce(new Error(errorMessage));
+
+    await expect(getAntragsSummaryList(1, 5)).rejects.toThrow(
+      "Fehler beim Laden der Antragsliste."
+    );
+  });
+});
