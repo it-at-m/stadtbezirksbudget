9e3dacafec266c4672c8857a4cc24d3457f4e293
Michael Huber <59747867+IamPekka058@users.noreply.github.com>
2025-11-04 13:48:12 +0100

    ♻️ Rename Antrag Update Schema

---
+++ stadtbezirksbudget-backend/src/main/resources/db/migration/schema/V003_0__AntragUpdate_schema.sql
@@ -0,0 +1,50 @@
+ALTER TABLE antrag
+    ADD aktenzeichen VARCHAR(255);
+
+ALTER TABLE antrag
+    ADD aktualisierung_art VARCHAR(255);
+
+ALTER TABLE antrag
+    ADD aktualisierung_datum TIMESTAMP WITHOUT TIME ZONE;
+
+ALTER TABLE antrag
+    RENAME COLUMN eingangsdatum to eingang_datum;
+
+ALTER TABLE antrag
+    ALTER COLUMN eingang_datum type TIMESTAMP WITHOUT TIME ZONE;
+
+ALTER TABLE antrag
+    ADD zammad_ticket_nr VARCHAR(255);
+
+ALTER TABLE finanzierung
+    ADD beantragtes_budget DECIMAL;
+
+ALTER TABLE finanzierung
+    ADD begruendung_eigenmittel VARCHAR(255);
+
+ALTER TABLE finanzierung
+    ADD kosten_anmerkung VARCHAR(255);
+
+ALTER TABLE finanzierung
+    ADD summe_ausgaben DECIMAL;
+
+ALTER TABLE finanzierung
+    ADD summe_finanzierungsmittel DECIMAL;
+
+ALTER TABLE projekt
+    ADD frist_bruch_begruendung VARCHAR(255);
+
+ALTER TABLE projekt
+    ADD ist_start_frist BOOLEAN;
+
+ALTER TABLE finanzierung
+    RENAME COLUMN ist_einladungs_foerderhinweis to ist_einladung_foerderhinweis;
+
+ALTER TABLE finanzierungsmittel
+    ALTER COLUMN betrag type decimal;
+
+ALTER TABLE voraussichtliche_ausgabe
+    ALTER COLUMN betrag type decimal;
+
+ALTER TABLE finanzierung
+    ALTER COLUMN bewilligter_zuschuss type decimal;
