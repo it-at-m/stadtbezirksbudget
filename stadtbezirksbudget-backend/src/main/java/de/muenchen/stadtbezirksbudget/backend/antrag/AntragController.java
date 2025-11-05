266a5f97054618d2ba10fdf4abdf524464b8ad4f
danielgully <74965377+danielgully@users.noreply.github.com>
2025-11-05 14:19:28 +0100

    :passport_control: Add PreAuthorize to controller

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/AntragController.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/AntragController.java
@@ -3,6 +3,8 @@ package de.muenchen.stadtbezirksbudget.backend.antrag;
 import de.muenchen.stadtbezirksbudget.backend.antrag.dto.AntragSummaryDTO;
 import de.muenchen.stadtbezirksbudget.backend.antrag.entity.Antrag;
 import java.util.List;
+
+import de.muenchen.stadtbezirksbudget.backend.security.Authorities;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -34,6 +37,7 @@ public class AntragController {
      */
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize(Authorities.ANTRAG_GET_SUMMARYPAGE)
     public Page<AntragSummaryDTO> getAntragSummaryPage(@PageableDefault final Pageable pageable) {
         final Page<Antrag> antragPage = antragService.getAntragPage(pageable);
         final List<AntragSummaryDTO> summaryList = antragPage.getContent().stream()
--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/security/Authorities.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/security/Authorities.java
@@ -15,6 +15,7 @@ public final class Authorities {
     public static final String THEENTITY_CREATE = "hasAnyRole('writer')";
     public static final String THEENTITY_UPDATE = "hasAnyRole('writer')";
     public static final String THEENTITY_DELETE = "hasAnyRole('writer')";
+    public static final String ANTRAG_GET_SUMMARYPAGE = "hasAnyRole('reader', 'writer')";
 
     // Permissions based auth
     // public static final String THEENTITY_GET = "hasAuthority('REFARCH_THEENTITY_READ')";
