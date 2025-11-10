d3cfce215804a3601d99df0136e880dccbbcfaac
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-10 14:42:43 +0100

    :wrench: Cluster ALTER Statements

--- stadtbezirksbudget-backend/src/main/resources/db/migration/schema/V003_5__Antrag_constraints_schema.sql
+++ stadtbezirksbudget-backend/src/main/resources/db/migration/schema/V003_5__Antrag_constraints_schema.sql
@@ -1,50 +1,26 @@
 ALTER TABLE antrag
-    ALTER COLUMN aktenzeichen SET NOT NULL;
-
-ALTER TABLE antrag
-    ALTER COLUMN aktualisierung_art SET NOT NULL;
-
-ALTER TABLE antrag
-    ALTER COLUMN aktualisierung_datum SET NOT NULL;
+    ALTER COLUMN aktenzeichen SET NOT NULL,
+    ALTER COLUMN aktualisierung_art SET NOT NULL,
+    ALTER COLUMN aktualisierung_datum SET NOT NULL,
+    ALTER COLUMN eingang_datum SET NOT NULL,
+    ALTER COLUMN zammad_ticket_nr SET NOT NULL;
 
 ALTER TABLE finanzierung
-    ALTER COLUMN beantragtes_budget SET NOT NULL;
-
-ALTER TABLE finanzierung
-    ALTER COLUMN begruendung_eigenmittel SET NOT NULL;
-
-ALTER TABLE antrag
-    ALTER COLUMN eingang_datum SET NOT NULL;
-
-ALTER TABLE projekt
-    ALTER COLUMN frist_bruch_begruendung SET NOT NULL;
-
-ALTER TABLE finanzierung
-    ALTER COLUMN ist_einladung_foerderhinweis SET NOT NULL;
-
-ALTER TABLE projekt
-    ALTER COLUMN ist_start_frist SET NOT NULL;
-
-ALTER TABLE finanzierung
-    ALTER COLUMN kosten_anmerkung SET NOT NULL;
-
-ALTER TABLE finanzierung
-    ALTER COLUMN summe_ausgaben SET NOT NULL;
-
-ALTER TABLE finanzierung
+    ALTER COLUMN beantragtes_budget SET NOT NULL,
+    ALTER COLUMN begruendung_eigenmittel SET NOT NULL,
+    ALTER COLUMN ist_einladung_foerderhinweis SET NOT NULL,
+    ALTER COLUMN kosten_anmerkung SET NOT NULL,
+    ALTER COLUMN summe_ausgaben SET NOT NULL,
     ALTER COLUMN summe_finanzierungsmittel SET NOT NULL;
 
-ALTER TABLE antrag
-    ALTER COLUMN zammad_ticket_nr SET NOT NULL;
+ALTER TABLE projekt
+    ALTER COLUMN frist_bruch_begruendung SET NOT NULL,
+    ALTER COLUMN ist_start_frist SET NOT NULL,
+    DROP CONSTRAINT UniqueProjekt,
+    ADD CONSTRAINT UniqueProjekt UNIQUE (titel, start, ist_start_frist, frist_bruch_begruendung, ende, beschreibung);
 
 ALTER TABLE finanzierungsmittel
     ALTER COLUMN betrag SET NOT NULL;
 
 ALTER TABLE voraussichtliche_ausgabe
     ALTER COLUMN betrag SET NOT NULL;
-
-ALTER TABLE projekt
-    DROP CONSTRAINT UniqueProjekt;
-
-ALTER TABLE projekt
-    ADD CONSTRAINT UniqueProjekt UNIQUE (titel, start, ist_start_frist, frist_bruch_begruendung, ende, beschreibung);
