8777b1ffef267528e48fbaf2db4517dd73480440
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-11 15:07:51 +0100

    :sparkles: Add ZammadEAIException for handling Zammad API errors

---
+++ stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/zammad/ZammadEAIException.java
@@ -0,0 +1,16 @@
+package de.muenchen.stadtbezirksbudget.cit_eai.zammad;
+
+import java.io.Serial;
+import java.nio.charset.Charset;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+public class ZammadEAIException extends WebClientResponseException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ZammadEAIException(final WebClientResponseException e, final String message) {
+        super(e.getStatusCode().value(), message, e.getHeaders(), e.getResponseBodyAsByteArray(), Charset.defaultCharset(), e.getRequest());
+    }
+
+}
