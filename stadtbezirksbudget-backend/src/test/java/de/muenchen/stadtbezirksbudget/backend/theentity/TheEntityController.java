364caf77f46e2df39bab602b0df532c98600f244
danielgully <74965377+danielgully@users.noreply.github.com>
2025-11-12 11:32:35 +0100

    :white_check_mark: Restore and reduce theentity-package for UnicodeFilterConfigurationTest

---
+++ stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/theentity/TheEntityController.java
@@ -0,0 +1,37 @@
+package de.muenchen.stadtbezirksbudget.backend.theentity;
+
+import de.muenchen.stadtbezirksbudget.backend.theentity.dto.TheEntityMapper;
+import de.muenchen.stadtbezirksbudget.backend.theentity.dto.TheEntityRequestDTO;
+import de.muenchen.stadtbezirksbudget.backend.theentity.dto.TheEntityResponseDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/theEntity")
+public class TheEntityController {
+
+    private final TheEntityService theEntityService;
+    private final TheEntityMapper theEntityMapper;
+
+    /**
+     * Create a new entity.
+     * Creates a new entity using the provided entity details.
+     *
+     * @param theEntityRequestDTO the details of the entity to create
+     * @return the created entity as a DTO
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TheEntityResponseDTO saveTheEntity(@Valid @RequestBody final TheEntityRequestDTO theEntityRequestDTO) {
+        return theEntityMapper.toDTO(theEntityService.createTheEntity(theEntityMapper.toEntity(theEntityRequestDTO)));
+    }
+}
---
+++ stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/theentity/TheEntityService.java
@@ -0,0 +1,18 @@
+package de.muenchen.stadtbezirksbudget.backend.theentity;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TheEntityService {
+
+    private final TheEntityRepository theEntityRepository;
+
+    public TheEntity createTheEntity(final TheEntity entity) {
+        log.debug("Create TheEntity {}", entity);
+        return theEntityRepository.save(entity);
+    }
+}
