5e045e155bf02abe8935677dcd04e65196cf8cec
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-13 08:07:44 +0100

    :recycle: Refactor ZammadEAIException to extend RuntimeException and enhance error handling

--- stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/zammad/ZammadEAIException.java
+++ stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/zammad/ZammadEAIException.java
@@ -1,20 +1,41 @@
 package de.muenchen.stadtbezirksbudget.cit_eai.zammad;
 
-import java.io.Serial;
 import java.nio.charset.Charset;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-public class ZammadEAIException extends WebClientResponseException {
+public class ZammadEAIException extends RuntimeException {
 
-    @Serial
-    private static final long serialVersionUID = 1L;
+    private final int statusCode;
+    private final String message;
+    private final String responseBody;
 
     public ZammadEAIException(final WebClientResponseException e, final String message) {
-        super(e.getStatusCode().value(), message, e.getHeaders(), e.getResponseBodyAsByteArray(), Charset.defaultCharset(), e.getRequest());
+        super(message, e);
+        this.statusCode = e.getStatusCode().value();
+        this.responseBody = e.getResponseBodyAsString();
+        this.message = message;
+    }
+
+    public ZammadEAIException(final WebClientResponseException e) {
+        super(e.getMessage());
+        this.statusCode = e.getStatusCode().value();
+        this.responseBody = e.getResponseBodyAsString(Charset.defaultCharset());
+        this.message = e.getMessage();
     }
 
     public ZammadEAIException(final String message) {
-        super(0, message, null, null, null, null);
+        super(message);
+        this.statusCode = -1;
+        this.message = message;
+        this.responseBody = "";
     }
 
+    @Override
+    public String toString() {
+        return "ZammadEAIException{" +
+                "statusCode=" + statusCode +
+                ", message='" + message + '\'' +
+                ", responseBody='" + responseBody + '\'' +
+                "} " + super.toString();
+    }
 }
