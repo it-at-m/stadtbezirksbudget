c34188d9f85e52268e3af4b82dd81d335285df26
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-18 12:12:52 +0100

    :sparkles: Add TestWebClientConfig for WebClient bean configuration in tests

---
+++ stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/TestWebClientConfig.java
@@ -0,0 +1,15 @@
+package de.muenchen.stadtbezirksbudget.cit_eai;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@TestConfiguration
+public class TestWebClientConfig {
+    @Bean
+    @Primary
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+}
