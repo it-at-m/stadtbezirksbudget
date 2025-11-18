b137e2bdfd131fe3f8443dbd3a7e6a0c05e14204
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-18 12:25:02 +0100

    :white_check_mark: Update KafkaProducerIntegrationTest to use multiple profiles and import TestWebClientConfig

--- stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/kafka/KafkaProducerIntegrationTest.java
+++ stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/kafka/KafkaProducerIntegrationTest.java
@@ -1,17 +1,20 @@
 package de.muenchen.stadtbezirksbudget.cit_eai.kafka;
 
+import static de.muenchen.stadtbezirksbudget.cit_eai.TestConstants.SPRING_NO_SECURITY_PROFILE;
+import static de.muenchen.stadtbezirksbudget.cit_eai.TestConstants.SPRING_TEST_PROFILE;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.muenchen.stadtbezirksbudget.cit_eai.TestConstants;
+import de.muenchen.stadtbezirksbudget.cit_eai.TestWebClientConfig;
 import java.util.UUID;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,7 +24,8 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @EmbeddedKafka(partitions = 1, topics = "${spring.kafka.template.default-topic}")
-@ActiveProfiles(TestConstants.SPRING_TEST_PROFILE)
+@ActiveProfiles(profiles = { SPRING_TEST_PROFILE, SPRING_NO_SECURITY_PROFILE })
+@Import(TestWebClientConfig.class)
 class KafkaProducerIntegrationTest {
 
     @Autowired
