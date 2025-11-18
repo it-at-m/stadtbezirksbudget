429f079a9507d4fcee7eb58ce9013ca931a110b3
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-18 12:08:42 +0100

    :wrench: Add SPRING_NO_SECURITY_PROFILE constant to TestConstants

--- stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/TestConstants.java
+++ stadtbezirksbudget-cit-eai/src/test/java/de/muenchen/stadtbezirksbudget/cit_eai/TestConstants.java
@@ -8,5 +8,6 @@ import lombok.NoArgsConstructor;
 public final class TestConstants {
 
     public static final String SPRING_TEST_PROFILE = "test";
+    public static final String SPRING_NO_SECURITY_PROFILE = "no-security";
 
 }
