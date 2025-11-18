85e5b2fbee22c9b10308160bb94bc425c62de1d8
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-18 14:29:45 +0100

    :recycle: Refactor ZammadEAIException to use StandardCharsets and improve toString method

--- stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/zammad/ZammadEAIException.java
+++ stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/zammad/ZammadEAIException.java
@@ -1,45 +1,43 @@
 package de.muenchen.stadtbezirksbudget.cit_eai.zammad;
 
 import java.io.Serial;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import lombok.Getter;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
+@Getter
 public class ZammadEAIException extends RuntimeException {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
     private final int statusCode;
-    private final String message;
     private final String responseBody;
 
     public ZammadEAIException(final WebClientResponseException e, final String message) {
         super(message, e);
         this.statusCode = e.getStatusCode().value();
         this.responseBody = e.getResponseBodyAsString();
-        this.message = message;
     }
 
     public ZammadEAIException(final WebClientResponseException e) {
         super(e.getMessage());
         this.statusCode = e.getStatusCode().value();
-        this.responseBody = e.getResponseBodyAsString(Charset.defaultCharset());
-        this.message = e.getMessage();
+        this.responseBody = e.getResponseBodyAsString(StandardCharsets.UTF_8);
     }
 
     public ZammadEAIException(final String message) {
         super(message);
         this.statusCode = -1;
-        this.message = message;
         this.responseBody = "";
     }
 
     @Override
     public String toString() {
         return "ZammadEAIException{" +
-                "statusCode=" + statusCode +
-                ", message='" + message + '\'' +
-                ", responseBody='" + responseBody + '\'' +
+                "statusCode=" + getStatusCode() +
+                ", message='" + getMessage() + '\'' +
+                ", responseBody='" + getResponseBody() + '\'' +
                 "} " + super.toString();
     }
 }
