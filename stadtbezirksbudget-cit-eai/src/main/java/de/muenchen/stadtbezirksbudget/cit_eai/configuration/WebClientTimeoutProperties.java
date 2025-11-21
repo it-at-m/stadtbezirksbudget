c61151f405cc63df1aabf17b7693d601bce0f3e1
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-21 08:03:05 +0100

    :recycle: Use duration in timeout properties

--- stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/configuration/WebClientTimeoutProperties.java
+++ stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/configuration/WebClientTimeoutProperties.java
@@ -1,11 +1,12 @@
 package de.muenchen.stadtbezirksbudget.cit_eai.configuration;
 
+import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "webclient.timeout")
 public record WebClientTimeoutProperties(
-        @DefaultValue("30") int readTimeout,
-        @DefaultValue("30") int writeTimeout,
-        @DefaultValue("30") int responseTimeout) {
+        @DefaultValue("30s") Duration readTimeout,
+        @DefaultValue("30s") Duration writeTimeout,
+        @DefaultValue("30s") Duration responseTimeout) {
 }
