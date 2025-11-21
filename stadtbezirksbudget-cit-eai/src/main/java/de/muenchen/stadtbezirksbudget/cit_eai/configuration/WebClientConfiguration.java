a411c4d54569182ea3542e6b462ebcefb491c063
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-21 08:12:19 +0100

    :recycle: Passing duration directly

--- stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/configuration/WebClientConfiguration.java
+++ stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/configuration/WebClientConfiguration.java
@@ -2,7 +2,6 @@ package de.muenchen.stadtbezirksbudget.cit_eai.configuration;
 
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +12,7 @@ public class WebClientConfiguration {
     @Bean
     public HttpClient configuredHttpClient(final WebClientTimeoutProperties timeoutProperties) {
         return HttpClient.create()
-                .responseTimeout(Duration.ofSeconds(timeoutProperties.responseTimeout().getSeconds()))
+                .responseTimeout(timeoutProperties.responseTimeout())
                 .doOnConnected(conn -> conn
                         .addHandlerLast(new ReadTimeoutHandler(timeoutProperties.readTimeout().getSeconds(), TimeUnit.SECONDS))
                         .addHandlerLast(new WriteTimeoutHandler(timeoutProperties.writeTimeout().getSeconds(), TimeUnit.SECONDS)));
