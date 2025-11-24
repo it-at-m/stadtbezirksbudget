88e6a76450545a84d3c26eba421322dd23825aef
danielgully <74965377+danielgully@users.noreply.github.com>
2025-11-24 18:40:05 +0100

    :sparkles: Introduce AntragFilterDTO which contains data, to filter applications

---
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/dto/AntragFilterDTO.java
@@ -0,0 +1,29 @@
+package de.muenchen.stadtbezirksbudget.backend.antrag.dto;
+
+import de.muenchen.stadtbezirksbudget.backend.antrag.entity.AktualisierungArt;
+import de.muenchen.stadtbezirksbudget.backend.antrag.entity.Status;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Data Transfer Object (DTO) for filtering Antrag entities.
+ *
+ * This DTO contains various filtering criteria that can be applied to query Antrag entities,
+ * including status, Bezirksausschuss number, date ranges, Antragsteller name, Projekt title,
+ * budget ranges, and update information.
+ */
+public record AntragFilterDTO(
+        List<Status> status,
+        List<Integer> bezirksausschussNr,
+        LocalDateTime eingangDatumVon,
+        LocalDateTime eingangDatumBis,
+        String antragstellerName,
+        String projektTitel,
+        BigDecimal beantragtesBudgetVon,
+        BigDecimal beantragtesBudgetBis,
+        boolean istFehlbetrag,
+        List<AktualisierungArt> aktualisierungArt,
+        LocalDateTime aktualisierungDatumVon,
+        LocalDateTime aktualisierungDatumBis) {
+}
