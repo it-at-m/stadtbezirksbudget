0d8bb6ec56e665986f1f1ba7e3957c43ba895c4e
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-25 09:19:15 +0100

    :memo: Add javadoc

--- stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/zammad/ZammadEAIException.java
+++ stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/zammad/ZammadEAIException.java
@@ -4,6 +4,9 @@ import java.io.Serial;
 import lombok.Getter;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
+/**
+ * Exception representing errors that occur when interacting with the Zammad EAI.
+ */
 @Getter
 public class ZammadEAIException extends RuntimeException {
 
@@ -12,21 +15,42 @@ public class ZammadEAIException extends RuntimeException {
 
     private final int statusCode;
 
+    /**
+     * Creates a new ZammadEAIException with the given WebClientResponseException and message.
+     *
+     * @param e the WebClientResponseException that caused this exception
+     * @param message the detail message
+     */
     public ZammadEAIException(final WebClientResponseException e, final String message) {
         super(message, e);
         this.statusCode = e.getStatusCode().value();
     }
 
+    /**
+     * Creates a new ZammadEAIException with the given WebClientResponseException.
+     *
+     * @param e the WebClientResponseException that caused this exception
+     */
     public ZammadEAIException(final WebClientResponseException e) {
         super(e.getMessage(), e);
         this.statusCode = e.getStatusCode().value();
     }
 
+    /**
+     * Creates a new ZammadEAIException with the given message.
+     *
+     * @param message the detail message
+     */
     public ZammadEAIException(final String message) {
         super(message);
         this.statusCode = -1;
     }
 
+    /**
+     * Returns a string representation of the ZammadEAIException.
+     *
+     * @return a string representation of the ZammadEAIException
+     */
     @Override
     public String toString() {
         return "ZammadEAIException{" +
