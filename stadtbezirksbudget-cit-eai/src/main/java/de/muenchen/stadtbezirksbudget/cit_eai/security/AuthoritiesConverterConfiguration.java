2da07301a45f857bf8358a21d39f4a29d2e158c3
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-11-27 13:11:02 +0100

    :passport_control: Add keycloak authorization to eai

---
+++ stadtbezirksbudget-cit-eai/src/main/java/de/muenchen/stadtbezirksbudget/cit_eai/security/AuthoritiesConverterConfiguration.java
@@ -0,0 +1,41 @@
+package de.muenchen.stadtbezirksbudget.cit_eai.security;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Configure how authorities are resolved.
+ * Either {@link KeycloakRolesAuthoritiesConverter} or with profile "userinfo-authorities"
+ * deprecated {@link UserInfoAuthoritiesConverter}.
+ */
+@Configuration
+@Profile("!no-security")
+public class AuthoritiesConverterConfiguration {
+    /**
+     * Creates a KeycloakRolesAuthoritiesConverter bean.
+     *
+     * @param securityProperties the security properties
+     * @return the KeycloakRolesAuthoritiesConverter
+     */
+    @Bean
+    @Profile("!userinfo-authorities")
+    public KeycloakRolesAuthoritiesConverter keycloakRolesAuthoritiesConverter(final SecurityProperties securityProperties) {
+        return new KeycloakRolesAuthoritiesConverter(securityProperties);
+    }
+
+    /**
+     * Creates a UserInfoAuthoritiesConverter bean.
+     *
+     * @param securityProperties the security properties
+     * @param restTemplateBuilder the rest template builder
+     * @return the UserInfoAuthoritiesConverter
+     */
+    @Bean
+    @Profile("userinfo-authorities")
+    public UserInfoAuthoritiesConverter userInfoAuthoritiesConverter(
+            final SecurityProperties securityProperties, final RestTemplateBuilder restTemplateBuilder) {
+        return new UserInfoAuthoritiesConverter(securityProperties.getUserInfoUri(), restTemplateBuilder);
+    }
+}
