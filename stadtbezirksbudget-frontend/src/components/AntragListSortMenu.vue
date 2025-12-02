5ae193d194c8fe3acfc8c8388c0af40fc13e2fd6
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-12-02 15:28:36 +0100

    :lipstick: Add AntragListSortMenu component

---
+++ stadtbezirksbudget-frontend/src/components/AntragListSortMenu.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { mdiSort } from "@mdi/js";
+
+import AntragListSort from "@/components/AntragListSort.vue";
+</script>
+
+<template>
+  <v-menu :close-on-content-click="false">
+    <template #activator="{ props }">
+      <v-btn
+        :icon="mdiSort"
+        aria-label="Anträge sortieren"
+        data-test="antrag-list-sort-open-btn"
+        v-bind="props"
+      />
+    </template>
+    <antrag-list-sort />
+  </v-menu>
+</template>
+
+<style scoped></style>
