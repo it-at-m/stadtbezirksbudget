2f5f50d6903e3eccc4f934dbba90b939c88a1913
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-12-03 14:18:12 +0100

    🏷️ Update type to remove redundancy

--- stadtbezirksbudget-frontend/src/types/AntragListSort.ts
+++ stadtbezirksbudget-frontend/src/types/AntragListSort.ts
@@ -1,142 +1,139 @@
+// Type definition of sorting diretion
 export type sortDirection = "asc" | "desc";
 
-export interface AntragListSort {
-  title: string;
-  sortBy: string;
-  sortDirection: sortDirection;
+// Interface for Antrag list sorting options
+export interface AntragListSortOption {
+    title: string;
+    sortBy: string;
+    sortDirection: sortDirection;
 }
 
-export const sortOptions: Record<string, AntragListSort[]> = {
-  status: [
-    {
-      title: "A-Z / Aufsteigend",
-      sortBy: "status",
-      sortDirection: "asc",
-    },
-    {
-      title: "Z-A / Absteigend",
-      sortBy: "status",
-      sortDirection: "desc",
-    },
-  ],
-  nummer: [
-    {
-      title: "Aufsteigend",
-      sortBy: "nummer",
-      sortDirection: "asc",
-    },
-    {
-      title: "Absteigend",
-      sortBy: "nummer",
-      sortDirection: "desc",
-    },
-  ],
-  aktenzeichen: [
-    {
-      title: "Aufsteigend",
-      sortBy: "aktenzeichen",
-      sortDirection: "asc",
-    },
-    {
-      title: "Absteigend",
-      sortBy: "aktenzeichen",
-      sortDirection: "desc",
-    },
-  ],
-  bezirk: [
-    {
-      title: "Aufsteigend",
-      sortBy: "bezirk",
-      sortDirection: "asc",
-    },
-    {
-      title: "Absteigend",
-      sortBy: "bezirk",
-      sortDirection: "desc",
-    },
-  ],
-  antragsdatum: [
-    {
-      title: "Zuerst hinzugefügt / Aufsteigend",
-      sortBy: "antragsdatum",
-      sortDirection: "asc",
-    },
-    {
-      title: "Zuletzt hinzugefügt / Absteigend",
-      sortBy: "antragsdatum",
-      sortDirection: "desc",
-    },
-  ],
-  antragsteller: [
-    {
-      title: "A-Z / Aufsteigend",
-      sortBy: "antragsteller",
-      sortDirection: "asc",
-    },
-    {
-      title: "Z-A / Absteigend",
-      sortBy: "antragsteller",
-      sortDirection: "desc",
-    },
-  ],
-  projekt: [
-    {
-      title: "A-Z / Aufsteigend",
-      sortBy: "projekt",
-      sortDirection: "asc",
-    },
-    {
-      title: "Z-A / Absteigend",
-      sortBy: "projekt",
-      sortDirection: "desc",
-    },
-  ],
-  beantragtesBudget: [
-    {
-      title: "Aufsteigend",
-      sortBy: "beantragtesBudget",
-      sortDirection: "asc",
-    },
-    {
-      title: "Absteigend",
-      sortBy: "beantragtesBudget",
-      sortDirection: "desc",
-    },
-  ],
-  art: [
-    {
-      title: "A-Z / Aufsteigend",
-      sortBy: "art",
-      sortDirection: "asc",
-    },
-    {
-      title: "Z-A / Absteigend",
-      sortBy: "art",
-      sortDirection: "desc",
-    },
-  ],
-  aktualisierungArt: [
-    {
-      title: "A-Z / Aufsteigend",
-      sortBy: "aktualisierungArt",
-      sortDirection: "asc",
-    },
-    {
-      title: "Z-A / Absteigend",
-      sortBy: "aktualisierungArt",
-      sortDirection: "desc",
-    },
-  ],
-  aktualisierungDatum: [
-    {
-      title: "Zuerst aktualisiert / Aufsteigend",
-      sortBy: "aktualisierungDatum",
-      sortDirection: "asc",
-    },
-    {
-      title: "Zuletzt aktualisiert / Absteigend",
-      sortBy: "aktualisierungDatum",
-      sortDirection: "desc",
-    },
-  ],
+// Type definition of AntragListSort based of {@link sortOptionsRecord}
+export type AntragListSort = Record<string, AntragListSortOption | undefined>;
+
+// Creates an empty AntragListSort object
+export const createEmptyListSort = (): AntragListSort => {
+    return Object.keys(sortOptionsRecord).reduce((acc, key) => {
+        acc[key] = undefined;
+        return acc;
+    }, {} as AntragListSort);
 };
+
+// Defines which sorting options are available for different values
+const sortOptionsRecord: Record<string, Omit<AntragListSortOption, "sortBy">[]> = {
+    status: [
+        {
+            title: "A-Z / Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Z-A / Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    nummer: [
+        {
+            title: "Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    aktenzeichen: [
+        {
+            title: "Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    bezirk: [
+        {
+            title: "Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    antragsdatum: [
+        {
+            title: "Zuerst hinzugefügt / Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Zuletzt hinzugefügt / Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    antragsteller: [
+        {
+            title: "A-Z / Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Z-A / Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    projekt: [
+        {
+            title: "A-Z / Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Z-A / Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    beantragtesBudget: [
+        {
+            title: "Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    art: [
+        {
+            title: "A-Z / Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Z-A / Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    aktualisierungArt: [
+        {
+            title: "A-Z / Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Z-A / Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+    aktualisierungDatum: [
+        {
+            title: "Zuerst aktualisiert / Aufsteigend",
+            sortDirection: "asc",
+        },
+        {
+            title: "Zuletzt aktualisiert / Absteigend",
+            sortDirection: "desc",
+        },
+    ],
+};
+
+// Populates the {@link sortOptionsRecord} with the corresponding key as sortBy param
+export const sortOptionsByField = (field: keyof typeof sortOptionsRecord): AntragListSortOption[] =>
+    sortOptionsRecord[field]?.map((value) => ({...value, sortBy: field})) || [];
+
