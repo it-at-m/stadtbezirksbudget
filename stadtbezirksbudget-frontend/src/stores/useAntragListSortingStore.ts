adddbee5f240c7e028b092d6c40f7a5245446701
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-12-03 14:17:01 +0100

    :sparkles: Add store for sorting

---
+++ stadtbezirksbudget-frontend/src/stores/useAntragListSortingStore.ts
@@ -0,0 +1,21 @@
+import {defineStore} from "pinia";
+import {ref} from "vue";
+import {type AntragListSort, createEmptyListSort} from "@/types/AntragListSort.ts";
+
+/**
+ * Pinia store for managing the sorting options applied to the Antrag list.
+ */
+export const useAntragListSortingStore = defineStore("antragListSort", () => {
+    // State: The current sorting options applied to the Antrag list.
+    const sorting = ref<AntragListSort>(createEmptyListSort());
+
+    /**
+     * Sets the sorting options for the Antrag list.
+     * @param payload - The new sorting options to apply.
+     */
+    function setListSorting(payload: AntragListSort): void {
+        sorting.value = payload;
+    }
+
+    return { sorting, setListSorting };
+});
