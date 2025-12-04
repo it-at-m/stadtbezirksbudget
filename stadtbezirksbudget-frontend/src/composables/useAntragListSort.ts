f9976fb3014d99b3be720365a866bf14166de479
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-12-04 11:23:16 +0100

    :art: Reformat code

--- stadtbezirksbudget-frontend/src/composables/useAntragListSort.ts
+++ stadtbezirksbudget-frontend/src/composables/useAntragListSort.ts
@@ -1,36 +1,44 @@
-import {ref} from "vue";
-import {type AntragListSort, type AntragListSortOption, createEmptyListSort} from "@/types/AntragListSort.ts";
-import {useAntragListSortingStore} from "@/stores/useAntragListSortingStore.ts";
+import type {
+  AntragListSort,
+  AntragListSortOption,
+} from "@/types/AntragListSort.ts";
+
+import { ref } from "vue";
+
+import { useAntragListSortingStore } from "@/stores/useAntragListSortingStore.ts";
+import { createEmptyListSort } from "@/types/AntragListSort.ts";
 
 /**
  * Composable for managing the Antrag list sorting options.
  * Provides reactive sorting and methods to update and reset them.
  */
 export function useAntragListSort() {
+  const sortingStore = useAntragListSortingStore();
 
-    const sortingStore = useAntragListSortingStore();
+  // The current applied sorting option
+  const sortOption = ref<AntragListSort>({ ...sortingStore.sorting });
 
-    // The current applied sorting option
-    const sortOption = ref<AntragListSort>({...sortingStore.sorting});
+  /**
+   * Updates the store with the current sorting options.
+   * @param newValue new sorting option
+   */
+  function updateSorting(newValue: AntragListSortOption) {
+    resetSorting();
 
-    /**
-     * Updates the store with the current sorting options.
-     * @param newValue new sorting option
-     */
-    function updateSorting(newValue: AntragListSortOption) {
-        resetSorting();
+    sortingStore.setListSorting({
+      ...createEmptyListSort(),
+      [newValue.sortBy]: newValue,
+    });
+    sortOption.value[newValue.sortBy] = newValue;
+  }
 
-        sortingStore.setListSorting({...createEmptyListSort(), [newValue.sortBy]: newValue})
-        sortOption.value[newValue.sortBy] = newValue;
-    }
+  /**
+   * Resets the current sorting options.
+   */
+  function resetSorting() {
+    sortingStore.setListSorting(createEmptyListSort());
+    sortOption.value = createEmptyListSort();
+  }
 
-    /**
-     * Resets the current sorting options.
-     */
-    function resetSorting() {
-        sortingStore.setListSorting(createEmptyListSort());
-        sortOption.value = createEmptyListSort();
-    }
-
-    return {sortOption, resetSorting, updateSorting};
+  return { sortOption, resetSorting, updateSorting };
 }
--- stadtbezirksbudget-frontend/src/stores/useAntragListSortingStore.ts
+++ stadtbezirksbudget-frontend/src/stores/useAntragListSortingStore.ts
@@ -1,21 +1,24 @@
-import {defineStore} from "pinia";
-import {ref} from "vue";
-import {type AntragListSort, createEmptyListSort} from "@/types/AntragListSort.ts";
+import type { AntragListSort } from "@/types/AntragListSort.ts";
+
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+import { createEmptyListSort } from "@/types/AntragListSort.ts";
 
 /**
  * Pinia store for managing the sorting options applied to the Antrag list.
  */
 export const useAntragListSortingStore = defineStore("antragListSort", () => {
-    // State: The current sorting options applied to the Antrag list.
-    const sorting = ref<AntragListSort>(createEmptyListSort());
+  // State: The current sorting options applied to the Antrag list.
+  const sorting = ref<AntragListSort>(createEmptyListSort());
 
-    /**
-     * Sets the sorting options for the Antrag list.
-     * @param payload - The new sorting options to apply.
-     */
-    function setListSorting(payload: AntragListSort): void {
-        sorting.value = payload;
-    }
+  /**
+   * Sets the sorting options for the Antrag list.
+   * @param payload - The new sorting options to apply.
+   */
+  function setListSorting(payload: AntragListSort): void {
+    sorting.value = payload;
+  }
 
-    return { sorting, setListSorting };
+  return { sorting, setListSorting };
 });
