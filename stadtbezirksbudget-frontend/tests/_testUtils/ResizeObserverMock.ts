1de06282a28475a438f23f553e72e01fea8ec29f
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-12-09 10:20:17 +0100

    :recycle: Add constructor to properly implement ResizeObserver interface

--- stadtbezirksbudget-frontend/tests/_testUtils/ResizeObserverMock.ts
+++ stadtbezirksbudget-frontend/tests/_testUtils/ResizeObserverMock.ts
@@ -4,4 +4,6 @@ export class ResizeObserverMock implements ResizeObserver {
   observe = vi.fn();
   unobserve = vi.fn();
   disconnect = vi.fn();
+
+  constructor(private _callback: ResizeObserverCallback) {}
 }
