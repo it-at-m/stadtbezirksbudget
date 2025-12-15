bef2795aecad6969ee6c843e46f01659573c4f41
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-12-15 12:12:21 +0100

    :rotating_light: Suppress pmd dataclass warning

--- stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/TestConstants.java
+++ stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/TestConstants.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@SuppressWarnings({ "PMD.TestClassWithoutTestCases" })
+@SuppressWarnings({ "PMD.TestClassWithoutTestCases", "PMD.DataClass" })
 public final class TestConstants {
 
     public static final String SPRING_TEST_PROFILE = "test";
