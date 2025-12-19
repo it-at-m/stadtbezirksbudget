642d2de92004bf91b57760d1c0d484b28807b7b2
bodmer02 <184080611+bodmer02@users.noreply.github.com>
2025-12-19 12:33:08 +0100

    :sparkles: Add zammad link to detailView

---
+++ stadtbezirksbudget-frontend/src/components/ZammadButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn
+    :href="zammadNrToLink(zammadNr)"
+    color="primary"
+    target="_blank"
+    variant="elevated"
+  >
+    Zammadticket öffnen
+  </v-btn>
+</template>
+
+<script lang="ts" setup>
+import { zammadNrToLink } from "@/util/formatter.ts";
+
+const { zammadNr } = defineProps<{ zammadNr: string }>();
+</script>
