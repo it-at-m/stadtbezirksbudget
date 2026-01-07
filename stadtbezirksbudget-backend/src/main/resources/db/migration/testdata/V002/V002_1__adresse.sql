72b59180c51ef393bbedf1f90ba6de6ad617a1b0
danielgully <74965377+danielgully@users.noreply.github.com>
2026-01-07 11:41:48 +0100

    :card_file_box: Remove old sql-files

--- stadtbezirksbudget-backend/src/main/resources/db/migration/testdata/V002/V002_1__adresse.sql
+++
@@ -1,6 +0,0 @@
-truncate table adresse, projekt, finanzierung, bearbeitungsstand, voraussichtliche_ausgabe, finanzierungsmittel, zahlungsempfaenger, bankverbindung, antrag, anderer_zuwendungsantrag RESTART IDENTITY CASCADE;
-
-INSERT INTO adresse (id, strasse, hausnummer, postleitzahl, ort)
-VALUES ('00000000-0000-0000-0000-000000000000', 'Musterstraße', '1A', '12345', 'Musterstadt'),
-       ('00000000-0000-0000-0000-000000000001', 'Beispielweg', '2', '67890', 'Beispielstadt'),
-       ('00000000-0000-0000-0000-000000000002', 'Testallee', '100', '54321', 'Teststadt');
