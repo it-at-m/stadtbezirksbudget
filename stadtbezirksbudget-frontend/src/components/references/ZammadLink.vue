6ec1cb4e8371d40a2697e3ef535991baa54c8358
bodmer02 <184080611+bodmer02@users.noreply.github.com>
2026-01-15 14:55:17 +0100

    :lipstick: Improve link style

--- stadtbezirksbudget-frontend/src/components/references/ZammadLink.vue
+++ stadtbezirksbudget-frontend/src/components/references/ZammadLink.vue
@@ -1,15 +1,27 @@
 <template>
   <a
     :href="zammadNrToLink(zammadNr)"
+    class="text-black text-decoration-none custom-link"
     rel="noopener noreferrer"
     target="_blank"
   >
     {{ zammadNr }}
+    <v-icon
+      :icon="mdiOpenInNew"
+      size="17"
+    />
   </a>
 </template>
 
 <script lang="ts" setup>
+import { mdiOpenInNew } from "@mdi/js";
+
 import { zammadNrToLink } from "@/util/formatter.ts";
 
 const { zammadNr } = defineProps<{ zammadNr: string }>();
 </script>
+<style scoped>
+.custom-link:hover {
+  text-decoration: underline !important;
+}
+</style>
