bd422a391d46e4bbbcef7f94564961a5ce1a47b3
bodmer02 <184080611+bodmer02@users.noreply.github.com>
2026-01-15 14:22:35 +0100

    :wrench: Add zammad ticket path to env for outsourcing

--- stadtbezirksbudget-frontend/env.d.ts
+++ stadtbezirksbudget-frontend/env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+import { ZAMMAD_TICKET_PATH } from "@/constants.ts";
+
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
@@ -10,6 +12,7 @@ interface ImportMetaEnv {
   readonly VITE_AD2IMAGE_URL: string;
   readonly VITE_APPSWITCHER_SERVER_URL: string;
   readonly VITE_ZAMMAD_BASE_URL: string;
+  readonly VITE_ZAMMAD_TICKET_PATH: string;
 }
 
 interface ImportMeta {
