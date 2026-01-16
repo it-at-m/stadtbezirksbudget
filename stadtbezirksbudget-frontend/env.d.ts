b8f9e1c3367165a67f59259688990cacbab038bd
bodmer02 <184080611+bodmer02@users.noreply.github.com>
2026-01-16 11:44:33 +0100

    :recycle: Insource ticketpath

--- stadtbezirksbudget-frontend/env.d.ts
+++ stadtbezirksbudget-frontend/env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="vite/client" />
 
-import { ZAMMAD_TICKET_PATH } from "@/constants.ts";
-
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
@@ -12,7 +10,6 @@ interface ImportMetaEnv {
   readonly VITE_AD2IMAGE_URL: string;
   readonly VITE_APPSWITCHER_SERVER_URL: string;
   readonly VITE_ZAMMAD_BASE_URL: string;
-  readonly VITE_ZAMMAD_TICKET_PATH: string;
 }
 
 interface ImportMeta {
--- stadtbezirksbudget-frontend/src/constants.ts
+++ stadtbezirksbudget-frontend/src/constants.ts
@@ -2,7 +2,7 @@ export const ROUTES_HOME = "home";
 export const ROUTES_DETAILS = "details";
 
 export const BACKEND = "/api/backend-service";
-export const ZAMMAD_TICKET_PATH = import.meta.env.VITE_ZAMMAD_TICKET_PATH;
+export const ZAMMAD_TICKET_PATH = "/#ticket/zoom/";
 export const ZAMMAD_BASE_URL = import.meta.env.VITE_ZAMMAD_BASE_URL;
 export const AD2IMAGE_URL = import.meta.env.VITE_AD2IMAGE_URL;
 export const APPSWITCHER_URL = import.meta.env.VITE_APPSWITCHER_URL;
