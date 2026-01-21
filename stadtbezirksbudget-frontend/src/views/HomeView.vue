5ff83cad5ff7ca00751fad24fa08d012ae1b539e
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-01-21 10:59:43 +0100

    :fire: Remove test buttons

--- stadtbezirksbudget-frontend/src/views/HomeView.vue
+++ stadtbezirksbudget-frontend/src/views/HomeView.vue
@@ -8,16 +8,6 @@
         <v-card rounded="lg"><antrag-list /></v-card>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col><p>Just for testing, needs to be removed:</p></v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-btn color="primary">Primary</v-btn>
-        <v-btn color="secondary">Secondary</v-btn>
-        <v-btn color="tertiary">Tertiary</v-btn>
-      </v-col>
-    </v-row>
   </v-container>
 </template>
 
