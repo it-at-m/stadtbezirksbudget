923b35f0b3a68562bca96270d6a67a820eb00ab5
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-01-23 10:36:14 +0100

    :sparkles: Only show list options on home page

--- stadtbezirksbudget-frontend/src/App.vue
+++ stadtbezirksbudget-frontend/src/App.vue
@@ -1,55 +1,8 @@
 <template>
   <v-app>
     <the-snackbar />
-    <v-app-bar color="primary">
-      <v-row align="center">
-        <v-col
-          class="d-flex align-center justify-start"
-          cols="3"
-        >
-          <v-app-bar-nav-icon @click.stop="toggleDrawer()" />
-          <router-link
-            class="text-decoration-none"
-            to="/"
-          >
-            <v-toolbar-title class="font-weight-bold">
-              <span class="text-white">Stadt</span>
-              <span class="text-primary-variant">bezirks</span>
-              <span class="text-white">budget</span>
-            </v-toolbar-title>
-          </router-link>
-        </v-col>
-        <v-col
-          class="d-flex align-center justify-center"
-          cols="6"
-        >
-          <antrag-list-search />
-        </v-col>
-        <v-col
-          class="d-flex align-center justify-end"
-          cols="3"
-        >
-          <antrag-list-sort-menu />
-          <antrag-list-filter-menu />
-          <app-switcher
-            v-if="appswitcherBaseUrl"
-            :base-url="appswitcherBaseUrl"
-            :icon="mdiApps"
-            :tags="['global']"
-          />
-          <v-btn
-            icon
-            variant="text"
-          >
-            <ad2-image-avatar
-              v-if="userStore.getUser !== null"
-              :username="userStore.getUser.username"
-            />
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer"></v-navigation-drawer>
+    <the-app-bar @toggle-navigation-drawer="toggleNavigationDrawer" />
+    <v-navigation-drawer v-model="navigationDrawer"></v-navigation-drawer>
     <v-main>
       <v-container fluid>
         <router-view v-slot="{ Component }">
@@ -63,26 +16,11 @@
 </template>
 
 <script lang="ts" setup>
-import { mdiApps } from "@mdi/js";
-import { AppSwitcher } from "@muenchen/appswitcher-vue";
 import { useToggle } from "@vueuse/core";
-import { onMounted } from "vue";
 
-import { getUser } from "@/api/user-client";
-import AntragListFilterMenu from "@/components/antragList/AntragListFilterMenu.vue";
-import AntragListSearch from "@/components/antragList/AntragListSearch.vue";
-import AntragListSortMenu from "@/components/antragList/AntragListSortMenu.vue";
-import Ad2ImageAvatar from "@/components/common/Ad2ImageAvatar.vue";
+import TheAppBar from "@/components/TheAppBar.vue";
 import TheSnackbar from "@/components/TheSnackbar.vue";
 import { useInitializeStores } from "@/composables/useInitializeStores.ts";
-import { APPSWITCHER_URL } from "@/constants";
-import { useUserStore } from "@/stores/useUserStore.ts";
-import User, { UserLocalDevelopment } from "@/types/User";
-
-const appswitcherBaseUrl = APPSWITCHER_URL;
-
-const userStore = useUserStore();
-const [drawer, toggleDrawer] = useToggle();
 
 useInitializeStores();
 
---
+++ stadtbezirksbudget-frontend/src/components/TheAppBar.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app-bar color="primary">
+    <v-row align="center">
+      <v-col
+        class="d-flex align-center justify-start"
+        cols="3"
+      >
+        <v-app-bar-nav-icon @click.stop="emit('toggleNavigationDrawer')" />
+        <router-link
+          class="text-decoration-none"
+          to="/"
+        >
+          <v-toolbar-title class="font-weight-bold">
+            <span class="text-white">Stadt</span>
+            <span class="text-primary-variant">bezirks</span>
+            <span class="text-white">budget</span>
+          </v-toolbar-title>
+        </router-link>
+      </v-col>
+      <v-col
+        class="d-flex align-center justify-center"
+        cols="6"
+      >
+        <antrag-list-search v-if="isHomePage" />
+      </v-col>
+      <v-col
+        class="d-flex align-center justify-end"
+        cols="3"
+      >
+        <antrag-list-sort-menu v-if="isHomePage" />
+        <antrag-list-filter-menu v-if="isHomePage" />
+        <app-switcher
+          v-if="APPSWITCHER_URL"
+          :base-url="APPSWITCHER_URL"
+          :icon="mdiApps"
+          :tags="['global']"
+        />
+        <v-btn
+          icon
+          variant="text"
+        >
+          <ad2-image-avatar
+            v-if="userStore.getUser !== null"
+            :username="userStore.getUser.username"
+          />
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+import { mdiApps } from "@mdi/js";
+import { AppSwitcher } from "@muenchen/appswitcher-vue";
+
+import AntragListFilterMenu from "@/components/antragList/AntragListFilterMenu.vue";
+import AntragListSearch from "@/components/antragList/AntragListSearch.vue";
+import AntragListSortMenu from "@/components/antragList/AntragListSortMenu.vue";
+import Ad2ImageAvatar from "@/components/common/Ad2ImageAvatar.vue";
+import { useRouteCheck } from "@/composables/useRouteCheck.ts";
+import { APPSWITCHER_URL } from "@/constants.ts";
+import { useUserStore } from "@/stores/useUserStore.ts";
+
+const emit = defineEmits<(e: "toggleNavigationDrawer") => void>();
+
+const userStore = useUserStore();
+const { isHomePage } = useRouteCheck();
+</script>
