a8dfd552bd6700f05c89f8d573c28ff31419c2de
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-02-09 15:26:21 +0100

    :white_check_mark: Generate eakteCooAdresse in antrag test builder

--- stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/antrag/integration/AntragBuilder.java
+++ stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/antrag/integration/AntragBuilder.java
@@ -54,6 +54,7 @@ public class AntragBuilder {
     private AktualisierungArt aktualisierungArt;
     private String zammadNr;
     private String aktenzeichen;
+    private String eakteCooAdresse;
     private String antragstellerName;
     private String projektTitel;
 
@@ -82,6 +83,7 @@ public class AntragBuilder {
         aktualisierungArt = AktualisierungArt.values()[RANDOM.nextInt(AktualisierungArt.values().length)];
         zammadNr = String.valueOf(RANDOM.nextInt(LIMIT));
         aktenzeichen = String.valueOf(RANDOM.nextInt(LIMIT));
+        eakteCooAdresse = String.valueOf(RANDOM.nextInt(LIMIT));
         antragstellerName = generateRandomUuidString();
         projektTitel = generateRandomUuidString();
     }
@@ -251,6 +253,7 @@ public class AntragBuilder {
                     .zammadTicketNr(zammadNr)
                     .aktualisierungDatum(aktualisierungDatum)
                     .aktenzeichen(aktenzeichen)
+                    .eakteCooAdresse(eakteCooAdresse)
                     .finanzierung(initializeFinanzierung(beantragtesBudget, finanzierungArt))
                     .projekt(initializeProjekt(projektTitel))
                     .antragsteller(antragsteller)
