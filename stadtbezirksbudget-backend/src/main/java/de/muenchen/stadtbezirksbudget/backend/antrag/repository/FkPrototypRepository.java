7d4a60aeaa7d902d268ffe18e5929c9ad89b0511
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-02-09 12:37:54 +0100

    :fire: Remove old unused files

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/repository/FkPrototypRepository.java
+++
@@ -1,7 +0,0 @@
-package de.muenchen.stadtbezirksbudget.backend.antrag.repository;
-
-import de.muenchen.stadtbezirksbudget.backend.antrag.entity.FkPrototyp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FkPrototypRepository extends JpaRepository<FkPrototyp, Long> {
-}
--- stadtbezirksbudget-backend/src/main/resources/db/migration/schema/V002__Fk_prototyp_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE fk_prototyp
-(
-    id                  BIGSERIAL PRIMARY KEY,
-    name_antragsteller  TEXT,
-    geldinstitut        TEXT,
-    bezirksausschuss_nr INTEGER,
-    created_at          TIMESTAMP DEFAULT now()
-);
