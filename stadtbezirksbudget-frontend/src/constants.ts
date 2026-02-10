cffb39fc1b5799f12a82e5e106455297cc8e266c
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-02-10 12:04:31 +0100

    :goal_net: Improve fallbacks if envs not set

--- stadtbezirksbudget-frontend/src/constants.ts
+++ stadtbezirksbudget-frontend/src/constants.ts
@@ -5,9 +5,9 @@ export const BACKEND = "/api/backend-service";
 export const ZAMMAD_TICKET_PATH = "/#ticket/zoom/";
 export const EAKTE_PATH = "/fsc/fscasp/content/bin/fscvext.dll?bx=";
 export const ZAMMAD_BASE_URL =
-  window.__RUNTIME_ENV__?.ZAMMAD_BASE_URL ?? "https://zammad.muenchen.de";
+  window.__RUNTIME_ENV__?.ZAMMAD_BASE_URL || "https://zammad.muenchen.de";
 export const EAKTE_BASE_URL =
-  window.__RUNTIME_ENV__?.EAKTE_BASE_URL ?? "https://akte.muenchen.de";
+  window.__RUNTIME_ENV__?.EAKTE_BASE_URL || "https://akte.muenchen.de";
 export const AD2IMAGE_URL = import.meta.env.VITE_AD2IMAGE_URL;
 export const APPSWITCHER_URL = import.meta.env.VITE_APPSWITCHER_URL;
 
