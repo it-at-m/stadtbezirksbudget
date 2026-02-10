f6154d8215dd7237dd129205114a19ecb3cbf6f5
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-02-10 11:57:12 +0100

    :bug: Keep env template for container restarts

--- stadtbezirksbudget-frontend/docker-entrypoint.sh
+++ stadtbezirksbudget-frontend/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 : "${ZAMMAD_BASE_URL}"
 : "${EAKTE_BASE_URL}"
 
-mv ./runtime-env.template.js ./runtime-env.js
+cp ../runtime-env.template.js ./runtime-env.js
 
 sed -i "s|\${ZAMMAD_BASE_URL}|$ZAMMAD_BASE_URL|g" ./runtime-env.js
 sed -i "s|\${EAKTE_BASE_URL}|$EAKTE_BASE_URL|g" ./runtime-env.js
