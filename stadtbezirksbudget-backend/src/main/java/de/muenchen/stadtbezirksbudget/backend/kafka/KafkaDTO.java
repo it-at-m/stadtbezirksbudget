08a6c5b4fc9c9ca9a2253d23deea628f01807670
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-02-10 16:24:12 +0100

    :sparkles: Validate KafkaDTO and replace old KafkaConsumerService.java

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/kafka/KafkaDTO.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/kafka/KafkaDTO.java
@@ -1,7 +1,14 @@
 package de.muenchen.stadtbezirksbudget.backend.kafka;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 /**
  * Dummy DTO for testing Kafka communication between CIT-EAI and backend modules.
  */
-public record KafkaDTO(String nameAntragsteller, String geldinstitut, Integer bezirksausschussNr) {
+public record KafkaDTO(
+        @NotBlank String nameAntragsteller,
+        @NotBlank String geldinstitut,
+        @NotNull @Positive int bezirksausschussNr) {
 }
