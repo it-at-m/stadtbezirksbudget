3169e765721b492c478046d42b82bdbfa4ac6f97
danielgully <74965377+danielgully@users.noreply.github.com>
2026-02-11 16:28:34 +0100

    :sparkles: Add @Formula-annotated fields

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/entity/Finanzierung.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/entity/Finanzierung.java
@@ -33,11 +33,16 @@ import org.hibernate.annotations.Formula;
 public class Finanzierung extends BaseEntity {
     @Serial
     private static final long serialVersionUID = 1L;
-
+    private static final String GESAMTKOSTEN_FORMULA = "SELECT COALESCE(SUM(a.betrag), 0) FROM voraussichtliche_ausgabe a WHERE a.finanzierung_id = id";
+    private static final String GESAMTMITTEL_FORMULA = "SELECT COALESCE(SUM(m.betrag), 0) FROM finanzierungsmittel m WHERE m.finanzierung_id = id";
     private static final String ART_FORMULA = "CASE " +
             "WHEN (SELECT COALESCE(SUM(a.betrag), 0) FROM voraussichtliche_ausgabe a WHERE a.finanzierung_id = id) > 5000 THEN 'FEHL' " +
             "WHEN (SELECT COALESCE(SUM(m.betrag), 0) FROM finanzierungsmittel m WHERE m.finanzierung_id = id) > 0 THEN 'FEHL' " +
             "ELSE 'FEST' END";
+    private static final String ZUWENIG_EIGENMITTEL_FORMULA = "SELECT CASE WHEN " +
+            "(SELECT COALESCE(SUM(m.betrag), 0) FROM finanzierungsmittel m WHERE m.finanzierung_id = id AND m.kategorie = 'EIGENMITTEL')" +
+            "< 0.25 * (" + GESAMTKOSTEN_FORMULA + ")" +
+            "THEN true ELSE false END";
 
     private boolean istProjektVorsteuerabzugsberechtigt;
     @NotNull private String kostenAnmerkung;
@@ -60,4 +65,14 @@ public class Finanzierung extends BaseEntity {
     @Formula(ART_FORMULA)
     @Enumerated(EnumType.STRING)
     private FinanzierungArt art;
+
+    @Formula(GESAMTKOSTEN_FORMULA)
+    private BigDecimal gesamtkosten;
+
+    @Formula(GESAMTMITTEL_FORMULA)
+    private BigDecimal gesamtmittel;
+
+    @Formula(ZUWENIG_EIGENMITTEL_FORMULA)
+    private boolean istZuwenigEigenmittel;
+
 }
