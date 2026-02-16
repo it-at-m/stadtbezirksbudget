9e01d4c54be704aed01e8afa0df2ffd6c5d1cbda
danielgully <74965377+danielgully@users.noreply.github.com>
2026-02-16 16:24:24 +0100

    :card_file_box: Refactor formulas and add brackets in them

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/entity/Finanzierung.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/entity/Finanzierung.java
@@ -33,16 +33,15 @@ import org.hibernate.annotations.Formula;
 public class Finanzierung extends BaseEntity {
     @Serial
     private static final long serialVersionUID = 1L;
-    private static final String GESAMTKOSTEN_FORMULA = "SELECT COALESCE(SUM(a.betrag), 0) FROM voraussichtliche_ausgabe a WHERE a.finanzierung_id = id";
-    private static final String GESAMTMITTEL_FORMULA = "SELECT COALESCE(SUM(m.betrag), 0) FROM finanzierungsmittel m WHERE m.finanzierung_id = id";
+    private static final String GESAMTKOSTEN_FORMULA = "(SELECT COALESCE(SUM(a.betrag), 0) FROM voraussichtliche_ausgabe a WHERE a.finanzierung_id = id)";
+    private static final String GESAMTMITTEL_FORMULA = "(SELECT COALESCE(SUM(m.betrag), 0) FROM finanzierungsmittel m WHERE m.finanzierung_id = id)";
     private static final String ART_FORMULA = "CASE " +
-            "WHEN (SELECT COALESCE(SUM(a.betrag), 0) FROM voraussichtliche_ausgabe a WHERE a.finanzierung_id = id) > 5000 THEN 'FEHL' " +
-            "WHEN (SELECT COALESCE(SUM(m.betrag), 0) FROM finanzierungsmittel m WHERE m.finanzierung_id = id) > 0 THEN 'FEHL' " +
+            "WHEN " + GESAMTKOSTEN_FORMULA + " > 5000 THEN 'FEHL' " +
+            "WHEN " + GESAMTMITTEL_FORMULA + " > 0 THEN 'FEHL' " +
             "ELSE 'FEST' END";
-    private static final String ZUWENIG_EIGENMITTEL_FORMULA = "SELECT CASE WHEN " +
+    private static final String ZUWENIG_EIGENMITTEL_FORMULA = "CASE WHEN " +
             "(SELECT COALESCE(SUM(m.betrag), 0) FROM finanzierungsmittel m WHERE m.finanzierung_id = id AND m.kategorie = 'EIGENMITTEL')" +
-            "< 0.25 * (" + GESAMTKOSTEN_FORMULA + ")" +
-            "THEN true ELSE false END";
+            " < 0.25 * " + GESAMTKOSTEN_FORMULA + " THEN true ELSE false END";
 
     private boolean istProjektVorsteuerabzugsberechtigt;
     @NotNull private String kostenAnmerkung;
