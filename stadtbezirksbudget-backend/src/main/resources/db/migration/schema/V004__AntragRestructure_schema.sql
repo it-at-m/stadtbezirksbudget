ad50596a990d9099e5247641ddee74e770ed7be5
danielgully <74965377+danielgully@users.noreply.github.com>
2026-02-20 17:09:08 +0100

    :card_file_box: Adapt table creation and inserting

--- stadtbezirksbudget-backend/src/main/resources/db/migration/schema/V004__AntragRestructure_schema.sql
+++ stadtbezirksbudget-backend/src/main/resources/db/migration/schema/V004__AntragRestructure_schema.sql
@@ -1,2 +1,11 @@
 ALTER TABLE antrag
     RENAME COLUMN bezirksausschuss_nr TO bezirksinformationen_ausschuss_nr;
+
+ALTER TABLE finanzierung
+    ADD COLUMN ist_person_vorsteuerabzugsberechtigt BOOLEAN NOT NULL;
+
+UPDATE finanzierung
+    SET ist_person_vorsteuerabzugsberechtigt = antrag.ist_person_vorsteuerabzugsberechtigt FROM antrag WHERE finanzierung.id = antrag.finanzierung_id;
+
+ALTER TABLE antrag
+    DROP COLUMN ist_person_vorsteuerabzugsberechtigt;
