7c1944ab069ab67b7cc90dcdb8c0aff4a89b0c69
danielgully <74965377+danielgully@users.noreply.github.com>
2026-02-24 15:52:19 +0100

    :construction: Update datatype of beschluss

---
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/dto/antrag_details/BezirksausschussDetailsDTO.java
@@ -0,0 +1,19 @@
+package de.muenchen.stadtbezirksbudget.backend.antrag.dto.antrag_details;
+
+import de.muenchen.stadtbezirksbudget.backend.antrag.entity.BeschlussStatus;
+import de.muenchen.stadtbezirksbudget.backend.common.ExcludedFromGeneratedCoverage;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * Data Transfer Object (DTO) representing the information of Bezirksinformationen.
+ */
+@ExcludedFromGeneratedCoverage(reason = "DTO is a pure data carrier (no logic) and therefore unreasonable to test.")
+public record BezirksausschussDetailsDTO(
+        int bezirksauschussNr,
+        LocalDate sitzungDatum,
+        String risNr,
+        BeschlussStatus beschluss,
+        BigDecimal bewilligteFoerderung,
+        LocalDate bescheidDatum) {
+}
