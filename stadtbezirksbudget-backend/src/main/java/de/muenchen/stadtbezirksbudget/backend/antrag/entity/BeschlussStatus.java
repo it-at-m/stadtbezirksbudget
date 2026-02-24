f43febee3ba57b6dc8d12a75395d607f08502eaf
danielgully <74965377+danielgully@users.noreply.github.com>
2026-02-24 16:35:47 +0100

    :bulb: Improve comments

--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/entity/BeschlussStatus.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/entity/BeschlussStatus.java
@@ -2,11 +2,13 @@ package de.muenchen.stadtbezirksbudget.backend.antrag.entity;
 
 /**
  * The BeschlussStatus represent following cases:
- * BEWILLIGT: bezirksinformationen.bewilligteFoerderung is equal to finanzierung.beantragtesBudget
- * TEILBEWILLIGT: bezirksinformationen.bewilligteFoerderung is unequal to
+ * <p>
+ * {@link BeschlussStatus#BEWILLIGT}: bezirksinformationen.bewilligteFoerderung is equal to
+ * finanzierung.beantragtesBudget
+ * {@link BeschlussStatus#TEILBEWILLIGT}: bezirksinformationen.bewilligteFoerderung is unequal to
  * finanzierung.beantragtesBudget AND > 0
- * ABGELEHNT: bezirksinformationen.bewilligteFoerderung is zero
- * LEER: bezirksinformationen.bewilligteFoerderung is null
+ * {@link BeschlussStatus#ABGELEHNT}: bezirksinformationen.bewilligteFoerderung is zero
+ * {@link BeschlussStatus#LEER}: bezirksinformationen.bewilligteFoerderung is null
  */
 public enum BeschlussStatus {
     BEWILLIGT,
--- stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/entity/FinanzierungArt.java
+++ stadtbezirksbudget-backend/src/main/java/de/muenchen/stadtbezirksbudget/backend/antrag/entity/FinanzierungArt.java
@@ -2,10 +2,13 @@ package de.muenchen.stadtbezirksbudget.backend.antrag.entity;
 
 /**
  * The FinanzierungArt represent following cases:
- * FEHL: The sum of all costs (VoraussichtlicheAusgabe), related to this finanzierung, are greater
- * then FEHL_ART_SCHWELLE OR the sum of all earnings (Finanzierungsmittel) is greater then zero
- * FEST: All other cases, meaning the sum of all earnings is zero and the sum of all costs are
- * smaller then FEHL_ART_SCHWELLE
+ * <p>
+ * {@link FinanzierungArt#FEHL}: The sum of all costs (VoraussichtlicheAusgabe), related to this
+ * finanzierung, are greater than FEHL_ART_SCHWELLE OR the sum of all earnings (Finanzierungsmittel)
+ * is greater than zero
+ * <p>
+ * {@link FinanzierungArt#FEST}: All other cases, meaning the sum of all earnings is zero and the
+ * sum of all costs are smaller than FEHL_ART_SCHWELLE
  */
 public enum FinanzierungArt {
     FEHL,
