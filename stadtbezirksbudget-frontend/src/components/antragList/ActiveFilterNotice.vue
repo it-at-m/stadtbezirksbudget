e82dc8c524c6587c1614093728cb1bd893272efb
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-02-25 11:48:31 +0100

    :sparkles: Add notice for active filters

---
+++ stadtbezirksbudget-frontend/src/components/antragList/ActiveFilterNotice.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-snackbar
+    v-model="show"
+    :timeout="-1"
+  >
+    Aktuell werden nicht alle Datensätze angezeigt, da Filter aktiviert sind.
+    <v-btn
+      color="primary"
+      data-test="filter-notice-close"
+      variant="text"
+      @click="show = false"
+    >
+      Schließen
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from "vue";
+
+const props = defineProps<{
+  active: boolean;
+}>();
+const show = ref<boolean>(props.active);
+watch(
+  () => props.active,
+  (active) => (show.value = active)
+);
+</script>
--- stadtbezirksbudget-frontend/src/components/antragList/AntragListFilterMenu.vue
+++ stadtbezirksbudget-frontend/src/components/antragList/AntragListFilterMenu.vue
@@ -6,6 +6,7 @@
     <template #activator="{ props }">
       <v-btn
         :icon="mdiFilter"
+        :variant="hasActiveFilters().value ? 'outlined' : undefined"
         aria-label="Anträge filtern"
         data-test="antrag-list-filter-open-btn"
         v-bind="props"
@@ -13,10 +14,15 @@
     </template>
     <antrag-list-filter />
   </v-menu>
+  <active-filter-notice :active="hasActiveFilters().value" />
 </template>
 
 <script lang="ts" setup>
 import { mdiFilter } from "@mdi/js";
 
+import ActiveFilterNotice from "@/components/antragList/ActiveFilterNotice.vue";
 import AntragListFilter from "@/components/antragList/AntragListFilter.vue";
+import { useAntragListFilter } from "@/composables/useAntragListFilter.ts";
+
+const { hasActiveFilters } = useAntragListFilter();
 </script>
