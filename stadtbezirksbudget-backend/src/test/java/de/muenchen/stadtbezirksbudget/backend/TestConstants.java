df5629251812ad1e2f54daf5403fb51114f968d1
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-03-02 14:06:11 +0100

    :speech_balloon: Update Kafka test profile constants in TestConstants.java

--- stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/TestConstants.java
+++ stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/TestConstants.java
@@ -11,8 +11,8 @@ public final class TestConstants {
 
     public static final String SPRING_NO_SECURITY_PROFILE = "no-security";
 
-    public static final String SPRING_KAFKA_TEST_PROFILE = "kafka-test";
-
     public static final String TESTCONTAINERS_POSTGRES_IMAGE = "postgres:17.4-alpine3.21";
+
+    public static final String TESTCONTAINERS_KAFKA_IMAGE = "confluentinc/cp-kafka:7.9.5";
 
 }
