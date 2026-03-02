bbba0d938eb5571543de979b29cf7c21f654334f
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2026-03-02 14:03:07 +0100

    :fire: Remove NoKafkaTestConfiguration

--- stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/antrag/integration/AntragFilteringIntegrationTest.java
+++ stadtbezirksbudget-backend/src/test/java/de/muenchen/stadtbezirksbudget/backend/antrag/integration/AntragFilteringIntegrationTest.java
@@ -35,10 +35,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @Transactional
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles(profiles = { SPRING_TEST_PROFILE, SPRING_NO_SECURITY_PROFILE })
@@ -47,8 +47,14 @@ class AntragFilteringIntegrationTest {
     @Container
     @ServiceConnection
     @SuppressWarnings("unused")
+    private static final ConfluentKafkaContainer KAFKA_CONTAINER = new ConfluentKafkaContainer(
+            DockerImageName.parse(TestConstants.TESTCONTAINERS_KAFKA_IMAGE));
+
+    @Container
+    @ServiceConnection
+    @SuppressWarnings("unused")
     private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>(
-            TestConstants.TESTCONTAINERS_POSTGRES_IMAGE);
+            DockerImageName.parse(TestConstants.TESTCONTAINERS_POSTGRES_IMAGE));
     @Autowired
     private AntragRepository antragRepository;
     @Autowired
