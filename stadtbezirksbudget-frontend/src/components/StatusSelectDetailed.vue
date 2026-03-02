87c39ad708b7c510a18c9499876efc7a13ca991d
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-03-02 15:23:58 +0100

    :lipstick: Display short and long status text in details view

---
+++ stadtbezirksbudget-frontend/src/components/StatusSelectDetailed.vue
@@ -0,0 +1,21 @@
+<template>
+  <status-select
+    v-model:menu="menu"
+    :item-props="itemProps"
+  />
+</template>
+
+<script lang="ts" setup>
+import type { StatusOption } from "@/types/Status.ts";
+
+import { ref } from "vue";
+
+import StatusSelect from "@/components/StatusSelect.vue";
+
+const menu = ref(false);
+
+const itemProps = (item: StatusOption) => ({
+  subtitle: item.longText,
+  title: menu.value ? item.shortText : `${item.shortText} - ${item.longText}`,
+});
+</script>
