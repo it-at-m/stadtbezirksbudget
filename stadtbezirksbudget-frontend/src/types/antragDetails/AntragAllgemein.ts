4c3f2fe3d99ab7a5465ae200912d8cb2720500d4
danielgully <74965377+danielgully@users.noreply.github.com>
2026-03-05 13:15:13 +0100

    :art: Reorder added types

--- stadtbezirksbudget-frontend/src/types/antragDetails/AntragAllgemein.ts
+++ stadtbezirksbudget-frontend/src/types/antragDetails/AntragAllgemein.ts
@@ -1,6 +1,6 @@
-import type { BeschlussStatus } from "@/types/antrag/BeschlussStatus.ts";
 import type { Status } from "@/types/antrag/Status.ts";
 import type { AntragAndererZuwendungsantrag } from "@/types/antragDetails/AntragAndererZuwendungsantrag.ts";
+import type { BeschlussStatus } from "@/types/antragDetails/enums/BeschlussStatus.ts";
 
 export interface AntragAllgemein {
   eingangDatum: string; // ISO date string from API
--- stadtbezirksbudget-frontend/src/types/antragDetails/AntragAndererZuwendungsantrag.ts
+++ stadtbezirksbudget-frontend/src/types/antragDetails/AntragAndererZuwendungsantrag.ts
@@ -1,4 +1,4 @@
-import type { AndererZuwendungsantragStatus } from "@/types/antrag/AndererZuwendungsantragStatus.ts";
+import type { AndererZuwendungsantragStatus } from "@/types/antragDetails/enums/AndererZuwendungsantragStatus.ts";
 
 export interface AntragAndererZuwendungsantrag {
   antragsdatum: string; // ISO date string from API
